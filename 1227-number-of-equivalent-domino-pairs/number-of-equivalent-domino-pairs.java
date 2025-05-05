8cd91589f264db81abdf6e6e6a93afe364c58ef5
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-05 13:31:42 +0530

    Time: 2 ms (98.26%) | Memory: 52.3 MB (99.25%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
@@ -5,8 +5,30 @@ class Solution {
         // first approach -> Brute Force approach ---> Gives Time limit exceeded
 
         //apporach 02 ---> using map 
-        Map<String , Integer> mp = new HashMap<>();
-        for(int i = 0 ; i < n ; i++){
+        // Map<String , Integer> mp = new HashMap<>();
+        // for(int i = 0 ; i < n ; i++){
+        //     int a = dominoes[i][0];
+        //     int b = dominoes[i][1];
+           
+        //     if(a > b){ 
+        //         //swap 
+        //         int temp = a ;
+        //         a = b ;
+        //         b = temp;
+        //     }
+        //     String st = a + "," + b;
+        //     mp.put(st , mp.getOrDefault(st , 0) + 1);
+        // }
+
+        // for(int val : mp.values()){
+        //     pairs += (val *(val - 1)) / 2;
+        // }
+        // return pairs;
+
+        //Approach 03 --> using constant space
+        int[] mp = new int[100];
+
+         for(int i = 0 ; i < n ; i++){
             int a = dominoes[i][0];
             int b = dominoes[i][1];
            
@@ -16,13 +38,14 @@ class Solution {
                 a = b ;
                 b = temp;
             }
-            String st = a + "," + b;
-            mp.put(st , mp.getOrDefault(st , 0) + 1);
+            int num = a * 10 + b; 
+            mp[num]++;
         }
 
-        for(int val : mp.values()){
-            pairs += (val *(val - 1)) / 2;
+        for(int i = 0 ; i < mp.length ; i++){
+            pairs += (mp[i] * (mp[i] - 1)) / 2;
         }
+
         return pairs;
 
     }
