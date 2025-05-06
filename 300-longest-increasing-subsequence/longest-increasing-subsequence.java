026a2bacc80a30d39e30414477e1fbc11d788623
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-06 13:04:29 +0530

    Time: 50 ms (35.04%) | Memory: 44 MB (94.22%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,52 @@
+class Solution {
+    int[][] memo;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return 1;
+
+        // memo = new int[2501][2501]; 
+        // for(int i = 0 ; i < n ; i++){
+        //     Arrays.fill(memo[i] , -1);
+        // }
+
+        // return solve(nums , 0 , -1, n);
+
+        //bottom up approach
+        int[] t = new int[n];
+        Arrays.fill(t , 1); // as every single element itself a single length subsequence 
+        //t[i] --> indicate that largest lis which is end at ith idx
+        int maxLIS = 1;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[j] < nums[i]){
+                    
+                    t[i] = Math.max(t[i] , (t[j] + 1));
+                    maxLIS = Math.max(maxLIS , t[i]);
+                }
+            }
+        }
+
+        return  maxLIS;
+    } 
+
+    public int solve(int[] nums , int idx , int prev , int n){ 
+
+        if(idx >= n) return 0;
+
+        if(prev != -1 && memo[idx][prev] != -1) return memo[idx][prev];
+
+        //take operation
+        int take = 0;
+        if(prev == -1 || (nums[prev] < nums[idx]))
+            take = 1 + solve(nums , idx + 1 , idx , n);
+
+        //skip operation
+        int skip = solve(nums , idx + 1 , prev , n);
+
+        if(prev != -1) memo[idx][prev] = Math.max(take , skip);
+
+        return  Math.max(take , skip);
+
+    }
+
+}
