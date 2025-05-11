20143f12d937b7edd2cfe25a54e16981eb052cbb
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-11 12:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (70.92%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -2,9 +2,25 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int n = arr.length;
 
-        for(int i = 0 ; i < n - 2 ; i++){
-            if(arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0){
-                return true;
+        //Approach 01 -> simple  
+        // for(int i = 0 ; i < n - 2 ; i++){
+        //     //if ith element even found then leave it 
+        //     if(arr[i] % 2 == 0) continue;
+
+        //     if(arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0){
+        //         return true;
+        //     }
+        // }
+
+        //aproach 02--> count consecutive streak
+        int oddCountStreak = 0;
+        for(int num : arr){
+            if(num % 2 != 0){
+                oddCountStreak++; 
+                //if 3 consecutive odd found
+                if(oddCountStreak == 3) return true;
+            }else{
+                oddCountStreak = 0;
             }
         }
         return false;
