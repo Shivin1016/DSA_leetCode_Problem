7f637c4ac5cfee60bf710e8a360675363fad4d2f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-14 13:29:27 +0530

    Time: 3 ms (8.60%) | Memory: 56 MB (96.06%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,29 +1,32 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int i = 0;
-        // while(i < k){
-        //     int idx = (i + k) % n;
-        //     int temp = nums[i];
-        //     nums[i] = nums[idx];
-        //     nums[idx] = temp;
-        //     i++;
-        // }
-        // if(n % 2 != 0){
-        //     //swap last and first ele
-        //     int temp = nums[0];
-        //     nums[0] = nums[n - 1];
-        //     nums[n - 1] = temp;
-        // }
-
-        //make k in range 
+         //make k in range 
         k =  k % n;
-        //reverse whole array
-        reverse(nums , 0 , n - 1); // t.c --> O(n/2)
 
-        //now reverse subarray of 0 to k - 1 and subarray of k to n - 1;
-        reverse(nums , 0 , k - 1);
-        reverse(nums , k , n - 1);
+        //Approach 02-> using extra space
+        List<Integer> ls = new ArrayList<>();
+        //store last k elements in ls
+        for(int i = n - k ; i < n ; i++){
+            ls.add(nums[i]);
+        }
+        //put 0 to k - 1 elem at right place
+        for(int i = n - k - 1 ; i >= 0 ; i--){
+            nums[i + k] = nums[i];
+        }
+        //now put k to n - 1 elem at right place
+        for(int i = 0 ; i < k ; i++){
+            nums[i] = ls.get(i);
+        }
+
+
+        //Approach 02-> using Reversing the array
+        //reverse whole array
+        // reverse(nums , 0 , n - 1); // t.c --> O(n/2)
+
+        // //now reverse subarray of 0 to k - 1 and subarray of k to n - 1;
+        // reverse(nums , 0 , k - 1);
+        // reverse(nums , k , n - 1);
 
     }
 
