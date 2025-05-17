3bd82ae7d5ec04d48a33f26a178356fc939921d8
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-17 10:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (91.69%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -2,18 +2,45 @@ class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
 
-        int[] mp = new int[3];
+        //using dutch flag algo
+        int low = 0 , mid = 0 , high = n -1;
 
-        for(int num : nums){
-            mp[num]++;
-        }
-        int j = 0;
-        for(int i = 0 ; i < 3 ; i++){
-            while(mp[i] != 0){
-                nums[j] = i;
-                j++;
-                mp[i]--;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                //swap low and mid
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++ ;
+                mid++;
+            }else if(nums[mid] == 1) mid++;
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
+
+
+
+        //using map
+        // int[] mp = new int[3];
+
+        // for(int num : nums){
+        //     mp[num]++;
+        // }
+
+        // int j = 0;
+
+        // for(int i = 0 ; i < 3 ; i++){
+        //     while(mp[i] != 0){
+        //         nums[j] = i;
+        //         j++;
+        //         mp[i]--;
+        //     }
+        // }
+
+        
     }
 }
