d9e003c2c0c0639e4c6ae91b025977be8a3c4455
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-17 12:55:35 +0530

    Time: 21 ms (54.32%) | Memory: 51.2 MB (20.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -6,12 +6,27 @@ class Solution {
         n2 = text2.length();
 
         //Approach 01--> recursion and memoization 
-        t = new int[1001][1001]; // memoization 
-        for(int i = 0 ; i < t.length ; i++){
-            Arrays.fill(t[i] , -1);
+        // t = new int[1001][1001]; // memoization 
+        // for(int i = 0 ; i < t.length ; i++){
+        //     Arrays.fill(t[i] , -1);
+        // }
+
+        // return solve(0 , 0 , text1.toCharArray() , text2.toCharArray());
+        //bottom up aprroach
+        t = new int[n1 + 1][n2 + 1];
+        // t[i][j] --> state defined that LCS between s1 of length i and s2 of length j
+        
+        for(int i = 1 ; i < n1 + 1 ; i++){
+            for(int j = 1 ; j < n2 + 1 ; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                }else{
+                    t[i][j] = Math.max(t[i][j - 1] , t[i - 1][j]);
+                }
+            }
         }
 
-        return solve(0 , 0 , text1.toCharArray() , text2.toCharArray());
+        return t[n1][n2];
     }
 
     public int solve(int i , int j , char[] text1 , char[] text2){
