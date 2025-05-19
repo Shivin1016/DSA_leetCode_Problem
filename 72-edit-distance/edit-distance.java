79bc6dab903d89c2bf8e049bb74d6dd488ec1edd
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-19 14:01:01 +0530

    Time: 205 ms (5.18%) | Memory: 55.5 MB (7.21%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,48 @@
+class Solution {
+    int n1 , n2;
+    int[][] t ;
+    public int minDistance(String word1, String word2) {
+        n1 = word1.length();
+        n2 = word2.length();
+
+        //memoiazation
+        t = new int[501][501];
+        for(int i = 0 ; i < t.length ; i++){
+            Arrays.fill(t[i] , -1);
+        }
+
+        return solve(0 , 0 , word1.toCharArray() , word2.toCharArray());
+
+    }
+
+    public int solve(int i , int j , char[] s1 , char[] s2){
+
+        if(i >= n1 && j >= n2) return 0;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(i >= n1 && j < n2){
+            return t[i][j] = (n2 - j);
+        }
+        if(i < n1 && j >= n2){
+            return t[i][j] = (n1 - i);
+        }
+
+        //character are already equal then no operation to be performed
+        if(s1[i] == s2[j]){
+            return t[i][j] = solve(i + 1 , j + 1 , s1 , s2);
+        }
+
+        //other wise we have to do three operations
+        //insert --> only jth pointer will move forward
+        int ins = 1 + solve(i , j + 1 , s1 , s2);
+
+        //deletion --> only ith pointer will move forward
+        int del = 1 + solve(i + 1 , j , s1 ,s2);
+
+        //replace--> both character move forward
+        int rep = 1 + solve(i + 1 , j + 1 , s1 , s2);
+
+        return t[i][j] = Math.min(ins , Math.min(del , rep));
+    }
+}
