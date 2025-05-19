e6a346fefea3ad6f4aae06092dc9b0f48d0f0a37
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-19 15:06:16 +0530

    Time: 5 ms (63.37%) | Memory: 45.3 MB (30.80%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -6,15 +6,36 @@ class Solution {
         n2 = word2.length();
 
         //memoiazation
-        t = new int[501][501];
-        for(int i = 0 ; i < t.length ; i++){
-            Arrays.fill(t[i] , -1);
+        // t = new int[501][501];
+        // for(int i = 0 ; i < t.length ; i++){
+        //     Arrays.fill(t[i] , -1);
+        // }
+
+        //bottom Up Approach->
+        t = new int[n1 + 1][n2 + 1];
+
+        for(int i = 0 ; i <= n1 ; i++){
+            for(int j = 0 ; j <= n2 ; j++){
+                if(i == 0 || j == 0){
+                    t[i][j] = i + j;
+                }
+                else if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    t[i][j] = t[i - 1][j - 1];
+                }
+                else{
+                    int ins = 1 + t[i][j - 1]; //insertion
+                    int del = 1 + t[i - 1][j]; // deletion
+                    int rep = 1 + t[i - 1][j - 1]; // replace
+                    t[i][j] = Math.min(ins , Math.min(del , rep));
+                }
+            }
         }
+        return t[n1][n2];
 
         //start from left to right
         // return solve(0 , 0 , word1.toCharArray() , word2.toCharArray());
         //start from right to left
-        return solve(n1, n2 , word1.toCharArray() , word2.toCharArray());
+        // return solve(n1, n2 , word1.toCharArray() , word2.toCharArray());
 
     }
 
@@ -26,12 +47,14 @@ class Solution {
 
         if(t[i][j] != -1) return t[i][j];
 
-        if(i <= 0 && j > 0){
-            return t[i][j] = j;
-        }
-        if(i > 0 && j <= 0){
-            return t[i][j] = i;
-        }
+        // if(i <= 0 && j > 0){
+        //     return t[i][j] = j;
+        // }
+        // if(i > 0 && j <= 0){
+        //     return t[i][j] = i;
+        // }
+        //shorter way ro right above conditons case
+        if(i == 0 || j == 0) return t[i][j] = i + j;
 
         //character are already equal then no operation to be performed
         if(s1[i - 1] == s2[j - 1]){
