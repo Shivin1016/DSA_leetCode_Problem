88f3ca3a13d77ea3818c507f66c9749f1422942b
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-05-23 17:49:05 +0530

    Time: 7 ms (43.42%) | Memory: 55.8 MB (18.42%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
@@ -3,7 +3,8 @@ class Solution {
         int m = edges.length;
         int n = nums.length;
 
-        int cnt = 0;
+        //approach 01-->
+        /* int cnt = 0;
         long idealSum = 0;  
         //if cnt is odd then take that xor values which give minimum nuksan
         long minNuksan = Integer.MAX_VALUE;
@@ -16,6 +17,7 @@ class Solution {
             }else{
                 idealSum += num;
             }
+            //we find nuksan because if cnt get odd so we have find its pair , so we have to find that node which have minimum nuksan on adding in sum 
             long nuksan = Math.abs(xor - num);
             minNuksan = (minNuksan > nuksan) ? nuksan : minNuksan;
             
@@ -27,6 +29,31 @@ class Solution {
         //if cnt is odd then remove last idex that we taken because lastIdx is added that is why cnt becomes odd  
         idealSum -= minNuksan;
         return idealSum ;
+        */
+
+        // Approach 02-->
+        long idealSum = 0;
+        for(int num : nums) idealSum += num;
+        int[] profit = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            int xor = nums[i] ^ k;
+            int fayada = xor - nums[i];
+            profit[i] = fayada;
+        }
+
+        //sort profit
+        Arrays.sort(profit);
+        int i = n - 1 , j = n - 2; 
+        while(j >= 0){ 
+            if(profit[i] + profit[j] > 0){
+                idealSum += profit[i] + profit[j];
+            }
+            j -= 2;
+            i -= 2;
+        }
+        return idealSum;
+
 
     }
 
