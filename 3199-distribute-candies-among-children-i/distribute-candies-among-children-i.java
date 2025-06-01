36fef3b4b85dc79e3629a4c1fb20b73de4604ede
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-06-01 18:34:42 +0530

    Time: 59 ms (7.41%) | Memory: 40.9 MB (49.54%) - LeetSync

---
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int distributeCandies(int n, int limit) {
+        //using recursive approach
+        
+        return solve(0 , n , limit); 
+    }
+
+    public int solve(int countOfChild , int n , int limit){
+        if(countOfChild == 3){
+            if(n == 0){
+                //all candies are distributed
+                return 1; // get one way to distributed candies among 3 children
+            }
+            return 0;
+        }
+
+        if(countOfChild > 3) return 0;
+
+        int ways = 0;
+
+        for(int assign = 0 ; assign <= limit ; assign++){
+            // we can distribute candies from 0 to limit
+            ways += solve(countOfChild + 1 , n - assign , limit); 
+        }
+
+        return ways;
+
+    }
+}
