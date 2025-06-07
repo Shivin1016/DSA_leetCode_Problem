5e9226571baa4cb20b2bdee9cffd3b3e535fa0b0
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-06-07 13:07:00 +0530

    Time: 62 ms (94.94%) | Memory: 45.8 MB (97.47%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -1,67 +1,47 @@
 class Solution {
     public String robotWithString(String s) {
-        // int n = s.length();
-
-        // //find minCHarTOright using preprocessing to right
-        // char[] minCharToRight = new char[n];
-        // char minCharIdx = s.charAt(n - 1);
-        // for(int i = n - 1 ; i >= 0 ; i--){
-        //     if(s.charAt(i) < minCharToRight.charAt()){
-        //         minCharIdx = i;
-        //         minCharToRight[i] = minCharIdx;
-        //     }else{
-        //         minCharToRight[i] = minCharIdx;
-        //     }
-        // }
-
-        // //traverse through s
-        // StringBuilder t = new StringBuilder();
-        // StringBuilder paper = new StringBuilder();
-        // for(int i = 0 ; i < n - 1; i++){
-        //     if(s.charAt(i) > s.charAt(minCharToRight[i + 1])){
-        //         t.append(s.charAt(i));
-        //     }else{
-        //         //if chota h then process in t
-        //         int j = t.length() - 1;
-        //         while(j < t.length() && t.charAt(j) < s.charAt(i)){
-        //             paper.append(t.charAt(j));
-        //             j--;
-        //         }
-        //     }
-        // }
-
-        // return paper.toString();
 
         int n = s.length();
 
-        // Array to store the smallest character from index i to the end
+        //find minCHarTOright using preprocessing to right
         char[] minCharToRight = new char[n];
+
+        char minChar = s.charAt(n - 1);
         minCharToRight[n - 1] = s.charAt(n - 1);
-        for (int i = n - 2; i >= 0; i--) {
-            minCharToRight[i] = (char) Math.min(s.charAt(i), minCharToRight[i + 1]);
+        for(int i = n - 2 ; i >= 0 ; i--){
+            minCharToRight[i] = (char)(Math.min(s.charAt(i) , minCharToRight[i + 1]));
         }
 
-        Deque<Character> stack = new ArrayDeque<>();
-        StringBuilder paper = new StringBuilder();
+        //traverse through s
+        StringBuilder t = new StringBuilder(); // t store reverse of the s String
 
-        int i = 0;
-        while (i < n) {
-            stack.push(s.charAt(i));
-            char minChar = (i + 1 < n) ? minCharToRight[i + 1] : s.charAt(i);
+        StringBuilder paper = new StringBuilder(); // robot write on the paper if it found that there is no such character lower then me in right hand side  
 
-            while (!stack.isEmpty() && stack.peek() <= minChar) {
-                paper.append(stack.pop());
+        int j = 0;
+        while(j < n){
+            t.append(s.charAt(j));
+
+            char miniChar = (j + 1 < n) ? minCharToRight[j + 1] : s.charAt(j);
+
+            int k = t.length() - 1;
+            while(k >= 0 && t.charAt(k) <= miniChar){
+                paper.append(t.charAt(k));
+                t.deleteCharAt(k);
+                k--;
             }
 
-            i++;
+            j++;
+        }
+        //write remaining character on paper from t
+        int i = t.length() - 1;
+        while(i >= 0){
+            paper.append(t.charAt(i));
+            t.deleteCharAt(i);
+            i--;
         }
 
-        // Empty the remaining characters from the stack
-        while (!stack.isEmpty()) {
-            paper.append(stack.pop());
-        }
 
-        return paper.toString();
+        return paper.toString(); 
 
 
     }
