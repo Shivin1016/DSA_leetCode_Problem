54a2c2e306f2cf35153f4ba10e9a01d61f82811f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-06-09 12:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (89.77%) - LeetSync

---
+++ Recursion/440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        //SKip those prefix number who have less then k numbers 
+
+        int curr = 1;
+        k -= 1; // as we started from first number 1 then we want k -1 more number
+        while(k > 0){ 
+            int countNum = count(curr , curr + 1 , n);
+            if(countNum <= k){
+                //we have to skip this prefix number because it have total numbers less then k 
+                // means we will find kth number in another prefix
+                curr++;
+                k -= countNum;
+            }else{
+                // we got our kth number in this curr prefix hance we have to go dipper 
+                curr *= 10; 
+                k -= 1; // we take only one prefix
+            } 
+        }
+        return curr;
+    }
+
+    public int count(long curr , long next , int limit){
+        int countNum = 0;
+        while(curr <= limit){
+            countNum += (next - curr);
+            //go dipper
+            curr *= 10; 
+            next *= 10;
+            //if the next is greater then limit then take only n + 1 next 
+            next = Math.min(next , (long)(limit + 1));
+        }
+        return countNum;
+    }
+}
