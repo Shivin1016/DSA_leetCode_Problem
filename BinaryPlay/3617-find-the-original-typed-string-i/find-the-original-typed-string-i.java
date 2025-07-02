c91e2e73b1f81f80c3014095d95ee1605cc5f9d4
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-02 15:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (78.23%) - LeetSync

---
+++ BinaryPlay/3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int possibleStringCount(String word) {
+
+        int n = word.length(); 
+
+        int possibelOriginalString = n; 
+
+        for(int i = 1 ; i < n ; i++){
+            if(word.charAt(i) != word.charAt(i - 1)){
+                possibelOriginalString--;
+            }
+        }
+
+        return possibelOriginalString;
+         
+
+    }
+}
