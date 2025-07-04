f8d76818c88d71d7c19d7f6d6d16319e374c7c58
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-04 11:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (75.43%) - LeetSync

--- BinaryPlay/3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ BinaryPlay/3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,21 +1,38 @@
 class Solution {
     public char kthCharacter(int k) {
-        List<Character> ans = new ArrayList<>();
-        ans.add('a');
 
-        int n = ans.size();
-        while(n < k){
+        //Approach 01 -> Using extra space --> T.C -> O(k) and S.C -> O(n)
+        // List<Character> ans = new ArrayList<>();
+        // ans.add('a');
 
-            for(int i = 0 ; i < n ; i++){
-                char ch = ans.get(i);
-                ch = (char)((ch + 1));
-                ans.add(ch);
-            }
+        // int n = ans.size();
+        // while(n < k){
 
-            n = ans.size();
+        //     for(int i = 0 ; i < n ; i++){
+        //         char ch = ans.get(i);
+        //         ch =  ch == 'z' ? 'a' : ch + 1;
+        //         ans.add(ch);
+        //     }
 
+        //     n = ans.size();
+
+        // }
+
+        // return ans.get(k - 1);
+
+        //Approach 02 -> without using extra space
+        //finding number of shifts , that how many times a shifts --> and shift is nothing but total number of set bits of (k - 1)
+
+        int shift = 0;
+        int idx = k - 1;
+
+        while(idx > 0){
+            if(idx % 2 == 1) shift++;
+            idx = idx / 2;
         }
 
-        return ans.get(k - 1);
+        return (char)('a' + shift);
+
+
     }
 }
