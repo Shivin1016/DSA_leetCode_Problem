8cdb2e96f766ecb03173897d48c2e15f31e99088
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-05 15:17:45 +0530

    Time: 1 ms (99.94%) | Memory: 43.4 MB (43.80%) - LeetSync

--- BinaryPlay/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ BinaryPlay/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -2,19 +2,40 @@ class Solution {
     public int findLucky(int[] arr) {
         int n = arr.length;
 
-        int[] frq = new int[501];
+        //Constant space complexity
+        // int[] frq = new int[501];
+
+        // for(int num : arr){
+        //     frq[num]++;
+        // }
+
+        // int lucky = -1;
+        // for(int i = 1 ; i < 501 ; i++){ 
+        //     if(i == frq[i]){
+        //         lucky = Math.max(lucky , frq[i]);
+        //     }
+        // }
+
+        // return lucky;
+
+        //Inplace O(1) space complxity
 
         for(int num : arr){
-            frq[num]++;
-        }
+            // num can be modified so taking AND with 2 ^ 16 gives real value 
+            int val = (num & 65535);
 
-        int lucky = -1;
-        for(int i = 1 ; i < 501 ; i++){ 
-            if(i == frq[i]){
-                lucky = Math.max(lucky , frq[i]);
+            if(val <= n){
+                arr[val - 1] += (1 << 16); // add 
             }
         }
 
-        return lucky;
+        for(int val = n ; val >= 1 ; val--){
+            //possibel values --> val
+            //gives frq count ->arr[val - 1] >> 16 --> right shift of number 16 times
+            if((arr[val - 1] >> 16) == val){
+                return val;
+            }
+        }
+        return -1;
     }
 }
