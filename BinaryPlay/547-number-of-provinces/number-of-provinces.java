2de094ad576e23e82bb5359e08b4111a3ac8648f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-05 17:05:32 +0530

    Time: 1 ms (87.65%) | Memory: 47.9 MB (46.23%) - LeetSync

--- BinaryPlay/547-number-of-provinces/number-of-provinces.java
+++ BinaryPlay/547-number-of-provinces/number-of-provinces.java
@@ -1,28 +1,30 @@
 class Solution {
+    int n ;
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+        n = isConnected.length;
+        
+        //using adjency list
+        // List<List<Integer>> adj = new ArrayList<>();
 
-        List<List<Integer>> adj = new ArrayList<>();
+        // for(int i = 0 ; i < n ; i++){
+        //     adj.add(new ArrayList<>());
+        // }
 
-        for(int i = 0 ; i < n ; i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(isConnected[i][j] == 1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = 0 ; j < n ; j++){
+        //         if(isConnected[i][j] == 1){
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
 
         boolean[] visited = new boolean[n];
         int countOfProvinces = 0;
 
         for(int i = 0 ; i < n ; i++){
             if(!visited[i]){
-                dfs(adj , i , visited);
+                dfs(isConnected , i , visited);
                 countOfProvinces++;
             }
         }
@@ -30,16 +32,24 @@ class Solution {
         return countOfProvinces;
     }
 
-    public void dfs(List<List<Integer>> adj , int u , boolean[] visited){
+    public void dfs(int[][] isConnected , int u , boolean[] visited){
         if(visited[u] == true) return;
 
         visited[u] = true;
 
-        for(int v : adj.get(u)){
-            if(!visited[v]){ 
-                dfs(adj , v , visited);
+        //without using adjency list
+        for(int v = 0 ; v < n ; v++){
+            if(!visited[v] && isConnected[u][v] == 1){
+                dfs(isConnected , v , visited);
             }
         }
+
+        //using adjency list
+        // for(int v : adj.get(u)){
+        //     if(!visited[v]){ 
+        //         dfs(adj , v , visited);
+        //     }
+        // }
 
 
     }
