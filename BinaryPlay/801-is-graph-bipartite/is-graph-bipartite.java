322ebc65c738ee6b429cce9dbeb715519711df45
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-08 13:01:13 +0530

    Time: 1 ms (62.15%) | Memory: 45.4 MB (19.67%) - LeetSync

--- BinaryPlay/801-is-graph-bipartite/is-graph-bipartite.java
+++ BinaryPlay/801-is-graph-bipartite/is-graph-bipartite.java
@@ -5,16 +5,57 @@ class Solution {
 
         int[] color = new int[n];
         Arrays.fill(color , -1);
- 
+
+
+        //Using DFS
+        // for(int i = 0 ; i < n ; i++){
+        //     if(color[i] == -1){
+        //         if(DFS(graph , i , color , 1) == false){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+
+        //using BFS 
         for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(DFS(graph , i , color , 1) == false){
+                if(BFS(graph , i , color , 1) == false){
                     return false;
                 }
             }
         }
 
         return true;
+    }
+
+    public boolean BFS(int[][] adj , int node , int[] color , int currColor){
+        
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(node);
+        //color the node
+        color[node] = currColor;
+
+        while(!que.isEmpty()){
+
+            int u = que.poll();
+
+            for(int v : adj[u]){
+                //same color matched
+                if(color[v] == color[u]){
+                    return false;
+                }
+
+                if(color[v] == -1){
+                    //color thr node
+                    color[v] = 1 - color[u];
+                    que.offer(v);
+                }
+            }
+        }
+
+        return true;
+
     }
 
     public boolean DFS(int[][] adj , int currNode , int[] color , int currColor){
