4f7028b23dcd38c91f97607352f6f4ece8cfb495
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-09 18:17:41 +0530

    Time: 4 ms (40.56%) | Memory: 57.6 MB (83.33%) - LeetSync

--- BinaryPlay/3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
+++ BinaryPlay/3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
@@ -2,27 +2,30 @@ class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
         int n = startTime.length;
 
-        List<Integer> gap = new ArrayList<>();
-        int prev = 0;
-        for(int i = 0 ; i < n ; i++){
-            gap.add(startTime[i] - prev);
-            prev = endTime[i];
-        }
-        //for last gap
-        gap.add(eventTime - prev);
+        List<Integer> gap = new ArrayList<>(); // store duration of freeGaps
+
+        //gap-> startTIme[i] - endTime[i - 1]
+        gap.add(startTime[0]);  // for start gap
+        for(int i = 1 ; i < n ; i++){
+            gap.add(startTime[i] - endTime[i - 1]); 
+        } 
+
+        //for last event
+        gap.add(eventTime - endTime[n -1]);
 
         //apply Siliding window for size K + 1
         int sum = 0 , i = 0 , j = 0;
         int res = 0;
-        while(j < gap.size()){ 
+        while(j < gap.size()){
 
             sum += gap.get(j);
 
-            if((j - i + 1) == (k + 1)){
-                res = Math.max(res , sum); 
+            while(i < n && (j - i + 1) > (k + 1)){
                 sum -= gap.get(i);
                 i++;
             }
+
+            res = Math.max(res , sum);
 
             j++;
         }  
