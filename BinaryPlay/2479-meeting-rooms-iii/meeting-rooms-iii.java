f938e43da427836437926b79402217ef7c7e0473
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-11 15:12:30 +0530

    Time: 79 ms (24.63%) | Memory: 97.6 MB (87.10%) - LeetSync

--- BinaryPlay/2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ BinaryPlay/2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,18 +1,19 @@
-class NewCompare implements Comparator<int[]>{
-    public int compare(int[] a , int[] b){
-        if(a[1] != b[1]){
-            return Integer.compare(a[1] , b[1]);
-        }
-        return Integer.compare(a[0] , b[0]);
+// class NewCompare implements Comparator<long[]>{
+//     public Long compare(long[] a , long[] b){
+//         if(a[0] != b[0]){
+//             return Long.compare(a[0] , b[0]);
+//         }
+//         return Long.compare(a[1] , b[1]);
 
-    }
-}
+//     }
+// }
 
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        
-        int len = meetings.length;
 
+        int len = meetings.length; 
+
+        /* Brute Force approach --> O(nlgn + n*m)
         //room number i will be last available at
         long[] lastRoomAvailableAt = new long[n]; 
 
@@ -20,9 +21,9 @@ class Solution {
         long[] roomUseCount = new long[n];
 
         //sort meetings array by first time
-        Arrays.sort(meetings , (a,b) -> a[0] - b[0]); 
+        Arrays.sort(meetings , (a,b) -> a[0] - b[0]);  //O(n Logn)
          
-        for(int i = 0 ; i < len ; i++){  
+        for(int i = 0 ; i < len ; i++){  //O(m)
 
             int endTime = meetings[i][1];
             int startTime = meetings[i][0];
@@ -32,7 +33,7 @@ class Solution {
             long earlyEndTime = Long.MAX_VALUE;
             boolean found = false;
 
-            for(int room = 0 ; room < n ; room++){  
+            for(int room = 0 ; room < n ; room++){  // O(n)
                 //Two Possibilites Are there --> either empty or filled if filled then check endTime
                 if(lastRoomAvailableAt[room] <= startTime){
                     //empty hai --> to direct meeting ko assign kr do
@@ -56,14 +57,64 @@ class Solution {
                 roomUseCount[roomEmptyFirst]++;
             }
         }
+        */
+
+        //OPtimal Approach --> Using heap 
+        
+        //sort meetings array by first time
+        Arrays.sort(meetings , (a,b) -> a[0] - b[0]);  //O(n Logn)
+
+        var usedRooms = new PriorityQueue<long[]>((a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+        
+        //Availability of rooms
+        var unUsedRooms = new PriorityQueue<Integer>();
+
+        for(int r = 0 ; r < n ; r++){
+            unUsedRooms.offer(r);
+        }
+
+        //count of rooms --> how many times they used
+        long[] roomUseCount = new long[n];
+
+        for(int i = 0 ; i < len ; i++){
+
+            int endTime = meetings[i][1];
+            int startTime = meetings[i][0];
+            int duration = endTime - startTime;
+
+            //check in heap that any meeting that will end before starting ith meeting
+            //pop it
+            while(!usedRooms.isEmpty() && usedRooms.peek()[0] <= startTime){ 
+                int room = (int) usedRooms.poll()[1]; 
+                unUsedRooms.offer(room);
+            }
+
+            //if heap empty then check for availibityRoom 
+            if(!unUsedRooms.isEmpty()){
+                int room = unUsedRooms.poll();
+                usedRooms.offer(new long[]{endTime , room});
+                roomUseCount[room]++;
+            }else{
+                //available room nahi mila
+                //then check for usedRoom jo sbse jaldi finish krde meeting ko
+                if(!usedRooms.isEmpty()){
+                    int room = (int)usedRooms.peek()[1];
+                    long end = usedRooms.poll()[0];
+                    usedRooms.offer(new long[]{(end + duration) , room});
+                    roomUseCount[room]++;
+                }
+            }
+
+        }        
 
         long maxRoomOccupied = 0;
-        int roomNumber = 0;
-        for(int i = 0 ; i < n ; i++){
+        int roomNumber = -1;
+        for(int i = 0 ; i < n ; i++){ // O(n)
             if(maxRoomOccupied < roomUseCount[i]){
                 maxRoomOccupied = roomUseCount[i];
                 roomNumber = i;
-            } 
+            }  
+            System.out.println(roomUseCount[i]);
         }
         return roomNumber;
 
