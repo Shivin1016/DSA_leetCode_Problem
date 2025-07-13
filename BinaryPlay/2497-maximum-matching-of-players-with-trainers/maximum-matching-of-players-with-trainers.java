4de5eec692b35edce45ec62b116f6dd458a091a9
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-13 12:09:17 +0530

    Time: 72 ms (5.41%) | Memory: 65.2 MB (33.25%) - LeetSync

--- BinaryPlay/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ BinaryPlay/2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,29 +1,30 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+
         // number of players
         int n = players.length;
         //number of trainers
         int m = trainers.length;
 
         //sort plyars in Ascending order
-        Arrays.sort(players);
+        Arrays.sort(players); // O(n lgn)
 
         //store trainers in pq
         var pq = new PriorityQueue<Integer>();
-        for(int trainer : trainers){
-            pq.add(trainer);
+        for(int trainerCapacity : trainers){ //O(m)
+            pq.add(trainerCapacity);
         }
 
         int maxMatching = 0;
-        for(int player : players){
-            
+        for(int playerAbility : players){ //O(n)
+
             if(pq.isEmpty()){
                 break;
             }
 
-            while(!pq.isEmpty()){
-                int trainer = pq.poll(); 
-                if(player <= trainer){
+            while(!pq.isEmpty()){ //O(logm)
+                int trainerCapacity = pq.poll(); 
+                if(playerAbility <= trainerCapacity){
                     maxMatching++;
                     break;
                 }
