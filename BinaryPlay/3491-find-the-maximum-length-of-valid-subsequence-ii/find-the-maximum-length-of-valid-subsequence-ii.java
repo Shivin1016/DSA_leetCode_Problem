e04a91b9120a4d1e405c23483721e32b9ec6ca96
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-18 00:36:34 +0530

    Time: 45 ms (36.08%) | Memory: 50.3 MB (80.41%) - LeetSync

--- BinaryPlay/3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
+++ BinaryPlay/3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -5,19 +5,32 @@ class Solution {
 
         //Initilise dp
         int[][] dp = new int[n][k];
+        for(int i = 0 ; i < n ; i++){
+            Arrays.fill(dp[i] , 1);
+        }
 
         // dp[i][mod] --> longest valid subsequence ending at index i with modulus mod
-        //starting all of zero
+        //starting all of 1
 
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
+        //total numbers of modulud can be k - 1
+        //col = 0 --> % k = 0;
+        //col = 1 --> % k = 1
+        // ....
+        //col = k - 1 --> % k = k - 1;
+
+        int ans = 1;
+        for(int i = 1 ; i < n ; i++){
             for(int j = 0 ; j < i ; j++){
+                //find modulus
                 int mod = (nums[i] + nums[j]) % k;
+
                 dp[i][mod] = Math.max(dp[i][mod] , dp[j][mod] + 1); 
+
                 ans = Math.max(dp[i][mod] , ans);
             }
         }
 
-        return ans + 1; // for pairs add 1
+        return ans; // for pairs add 1
+        
     }
 }
