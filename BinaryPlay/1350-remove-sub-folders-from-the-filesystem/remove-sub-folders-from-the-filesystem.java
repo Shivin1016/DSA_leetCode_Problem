4ddece1a1fdbad69d93cf3955631e5af272455a4
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-19 17:49:02 +0530

    Time: 39 ms (87.11%) | Memory: 55.2 MB (84.38%) - LeetSync

--- BinaryPlay/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ BinaryPlay/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -5,18 +5,24 @@ class Solution {
 
         //Sort them 
         Arrays.sort(folder); 
+
         List<String> ans = new ArrayList<>();
 
         String prev = folder[0]; 
-        ans.add(folder[0]);
+
+        ans.add(folder[0]); 
 
         for(int i = 1; i < n ; i++){
-            String str = folder[i]; 
-            //whole folder executed --> no subFolder 
+            
+            String str = folder[i];  
+
+            //prev folder + / is found in starting(idex == 0) it means subFolder else not 
+
             if(str.indexOf(prev + '/') != 0){
                 ans.add(str);
                 prev = str;
             }
+
         } 
 
         return ans;
