c7b4d9551a8bbb1a502c426a8a79b830f47f4d94
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-23 18:31:30 +0530

    Time: 8 ms (93.99%) | Memory: 61.7 MB (6.22%) - LeetSync

--- BinaryPlay/1813-maximum-erasure-value/maximum-erasure-value.java
+++ BinaryPlay/1813-maximum-erasure-value/maximum-erasure-value.java
@@ -3,7 +3,8 @@ class Solution {
         int n = nums.length;
         int i = 0 , j = 0;
 
-        Set<Integer> st = new HashSet<>();
+        //using Set
+        /*Set<Integer> st = new HashSet<>();
         long sum = 0 , maxSum = 0;
 
         while(j < n){
@@ -21,5 +22,32 @@ class Solution {
             j++;
         }
         return (int)maxSum;
+        */
+
+        //using map store value with idx for jumping i to the last seen idx
+        //find commulative sum --> when jumping from ith to last seen value idx 
+        int[] cumSum = new int[n];
+        cumSum[0] = nums[0];
+        for(int k = 1 ; k < n ; k++){
+            cumSum[k] = cumSum[k - 1] + nums[k];
+        }
+        int[] mp = new int[10001];
+        //intially all have -1 idx
+        Arrays.fill(mp , -1);
+        int maxSum = 0;
+        while(j < n){
+
+            i = Math.max(i , mp[nums[j]] + 1); // jumping to i so that i to j is valid
+
+            //subArray sum to ith to jth
+            int jthSum = cumSum[j];
+            int ithSum = i - 1 < 0 ? 0 : cumSum[i - 1]; 
+
+            maxSum = Math.max(maxSum , jthSum - ithSum);
+
+            mp[nums[j]] = j;
+            j++; 
+        }
+        return maxSum;
     }
 }
