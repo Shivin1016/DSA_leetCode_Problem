8d8394359e8e3dc8c5189c66131153059d50de9d
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-07-25 14:50:14 +0530

    Time: 2 ms (74.06%) | Memory: 42 MB (99.38%) - LeetSync

--- BinaryPlay/3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
+++ BinaryPlay/3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -3,24 +3,25 @@ class Solution {
         int n = nums.length;
 
         //if all values are negative then greater value will be athe answer
-        int maxVal = Integer.MIN_VALUE;
-        for(int num : nums){
-            maxVal = Math.max(maxVal , num);
-        }
-        if(maxVal < 0) return maxVal;
+        // int maxVal = Integer.MIN_VALUE;
+        // for(int num : nums){
+        //     maxVal = Math.max(maxVal , num);
+        // }
+        // if(maxVal < 0) return maxVal;
 
         var st = new HashSet<Integer>();
-        int sum = 0 , res = 0;
+        int sum = 0 , neg = Integer.MIN_VALUE;
         for(int i = 0 ; i < n ; i++){
             if(nums[i] <= 0 || st.contains(nums[i])){
+                //store negative max value --> if all the values are negative then we use it
+                neg = Math.max(neg , nums[i]);
                 continue; // skip those value which are negative or already present in set
             }
             //else take values and add in set
-            sum += nums[i];
-            res = Math.max(res , sum);
+            sum += nums[i]; 
             st.add(nums[i]);
         }
 
-        return res;
+        return sum == 0 ? neg : sum;
     }
 }
