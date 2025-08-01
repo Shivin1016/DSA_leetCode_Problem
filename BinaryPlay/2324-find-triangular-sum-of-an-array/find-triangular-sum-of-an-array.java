4e216b8eff50c8eee0eb880ec1b2d333c3de5c12
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-01 14:33:30 +0530

    Time: 42 ms (88.46%) | Memory: 44.1 MB (96.25%) - LeetSync

--- BinaryPlay/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ BinaryPlay/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,14 +1,22 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        while(nums.length > 1){
-            int newLen = nums.length - 1;
-            int[] newNums = new int[newLen];
-            int j = 0;
-            while(j < newLen){
-                newNums[j] = (nums[j] + nums[j + 1]) % 10;
-                j++;
+        // while(nums.length > 1){
+        //     int newLen = nums.length - 1;
+        //     int[] newNums = new int[newLen];
+        //     int j = 0;
+        //     while(j < newLen){
+        //         newNums[j] = (nums[j] + nums[j + 1]) % 10;
+        //         j++;
+        //     }
+        //     nums = newNums;
+        // }
+
+        int n = nums.length;
+        while(n > 1){  
+            for(int i = 0 ; i < n - 1 ; i++){
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
             }
-            nums = newNums;
+            n--;
         }
         return nums[0];
     }
