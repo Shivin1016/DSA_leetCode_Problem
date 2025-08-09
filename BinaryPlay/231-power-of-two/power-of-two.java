e8fbe5a10754e9eb6dc2d7786206b9f1001ec7f8
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-09 19:35:20 +0530

    Time: 1 ms (35.75%) | Memory: 40.8 MB (63.12%) - LeetSync

--- BinaryPlay/231-power-of-two/power-of-two.java
+++ BinaryPlay/231-power-of-two/power-of-two.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isPowerOfTwo(int n){
 
-        for(int i = 0 ; i < 32 ; i++){
-
-            double power = Math.pow(2 , i); 
-            if((double)(n) == power){
-                return true;
-            }
+        int count = 0 ;
+        if(n < 0){
+            return false;
         }
 
-        return false;
+        while(n != 0){
+            if( (n&1) == 1 ) count++;
+            n = n >> 1;
+        }
+
+        return count == 1;
+
+        // for(int i = 0 ; i < 32 ; i++){
+        //     double power = Math.pow(2 , i); 
+        //     if((double)(n) == power){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
     }
 }
