1ada4d8689fd58531058e0565ba84fd3c6baa1dc
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-15 14:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (89.50%) - LeetSync

--- BinaryPlay/342-power-of-four/power-of-four.java
+++ BinaryPlay/342-power-of-four/power-of-four.java
@@ -2,9 +2,10 @@ class Solution {
     public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
         
-        //approach 
-        int x = (int) (Math.log(n) / Math.log(4));
+        //approach  
+        if((n & (n - 1)) == 0 && (n - 1) % 3 == 0) return true;
 
-        return n == Math.pow(4 , x);   
+        return false;
+
     }
 }
