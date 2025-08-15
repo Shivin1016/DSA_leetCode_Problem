57893ddbb111d06596d1b30f421a74297820871c
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-15 13:41:39 +0530

    Time: 8 ms (83.18%) | Memory: 44.2 MB (49.38%) - LeetSync

--- BinaryPlay/326-power-of-three/power-of-three.java
+++ BinaryPlay/326-power-of-three/power-of-three.java
@@ -10,14 +10,18 @@ class Solution {
 
         if(n == 0) return false;
 
+        //Approach 04 -> divide max power of 3 which fit into 32 bit integer by n if it's modulo become zero , return ture
+
+        int pow = (int)Math.pow(3 , 19); // maximum power fit into 32 bit
+        return n > 0 && pow % n == 0;
+
         //using log solution -> basic mathmatics
         /*
-          suppose n can be -> 3 ^ x = n -> take log on both side 
+          suppose n can be -> 3 ^ x = n -> take log on both side(take base 10) 
           log(3^x) = log(n) -> x = log(n) / log(3) --> if x is integer then return true
         */
-
-        double x = ((Math.log10(n)) / (Math.log10(3)));
-        return x == (int)x;
+        // double x = ((Math.log10(n)) / (Math.log10(3)));
+        // return x == (int)x;
 
         //recusive solution
         // return check(n);
