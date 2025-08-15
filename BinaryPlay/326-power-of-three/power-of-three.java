c5328a0b1808bc2aa308499921cd64c2a1555a46
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-15 13:38:25 +0530

    Time: 8 ms (83.18%) | Memory: 44.1 MB (65.40%) - LeetSync

--- BinaryPlay/326-power-of-three/power-of-three.java
+++ BinaryPlay/326-power-of-three/power-of-three.java
@@ -2,16 +2,25 @@ class Solution {
     public boolean check(int n){
         if(n == 0) return false;
 
-        if(n == 1) return true;
+        if(n == 1) return true; 
 
-        if(n % 3 != 0) return false;
-
-        return check(n / 3);
+        return n % 3 == 0 && check(n / 3);
     }
     public boolean isPowerOfThree(int n) {
 
+        if(n == 0) return false;
+
+        //using log solution -> basic mathmatics
+        /*
+          suppose n can be -> 3 ^ x = n -> take log on both side 
+          log(3^x) = log(n) -> x = log(n) / log(3) --> if x is integer then return true
+        */
+
+        double x = ((Math.log10(n)) / (Math.log10(3)));
+        return x == (int)x;
+
         //recusive solution
-        return check(n);
+        // return check(n);
 
         //iterative solution
         /*if(n == 0) return false;
