481f9fc6b8235e8bcbc8daf09d06c0e638526d7e
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-16 15:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (28.34%) - LeetSync

--- BinaryPlay/2496-count-days-spent-together/count-days-spent-together.java
+++ BinaryPlay/2496-count-days-spent-together/count-days-spent-together.java
@@ -1,8 +1,22 @@
 import java.time.LocalDate;
 class Solution {
+    public int countDay(int month , int day){
+        int[] numDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days = 0;
+        int m = 0;
+        //as index is 0 based so take month - 1
+        while(m != (month - 1)){
+            days += numDays[m];
+            m++;
+        }
+        days += day; // for last month where month apppear add its day
+        return days;
+    }
     public int countDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob) {
 
+        //using java inbuilt package
         //alice incomin ang outgoing dates
+        /*
         LocalDate d1 = LocalDate.of(2021 , Integer.parseInt(arriveAlice.substring(0 , 2)) , Integer.parseInt(arriveAlice.substring(3 , 5)));
         LocalDate d2 = LocalDate.of(2021 , Integer.parseInt(leaveAlice.substring(0 , 2)) , Integer.parseInt(leaveAlice.substring(3 , 5)));
 
@@ -15,12 +29,18 @@ class Solution {
         long aliceLeave = d2.toEpochDay();
         long bobArrive = d3.toEpochDay();
         long bobLeave = d4.toEpochDay();
+        */
 
-        //no days together found
-        if(aliceArrive > bobLeave || bobArrive > aliceLeave){
-            return 0;
-        }
+        int aliceArrive = countDay(Integer.parseInt(arriveAlice.substring(0 , 2)) , Integer.parseInt(arriveAlice.substring(3 , 5)));
+        int aliceLeave = countDay(Integer.parseInt(leaveAlice.substring(0 , 2)) , Integer.parseInt(leaveAlice.substring(3 , 5)));
+        int bobArrive = countDay(Integer.parseInt(arriveBob.substring(0 , 2)) , Integer.parseInt(arriveBob.substring(3 , 5)));
+        int bobLeave = countDay(Integer.parseInt(leaveBob.substring(0 , 2)) , Integer.parseInt(leaveBob.substring(3 , 5)));
+         
+        int arrive = Math.max(aliceArrive , bobArrive);
+        int left = Math.min(bobLeave , aliceLeave);
 
-        return (int) (Math.min(bobLeave , aliceLeave) - Math.max(aliceArrive , bobArrive)) + 1;
+        if(arrive > left) return 0;
+
+        return left - arrive + 1;
     }
 }
