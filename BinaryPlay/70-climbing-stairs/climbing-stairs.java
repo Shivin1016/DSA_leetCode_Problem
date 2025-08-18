fa19de6cace2b11e33a608122b33a57517ad1235
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-18 16:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.80%) - LeetSync

--- BinaryPlay/70-climbing-stairs/climbing-stairs.java
+++ BinaryPlay/70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
         if(n <= 1) return 1;
-        //bottom up approach
-
-        int[] dp = new int[n + 1]; //tell us total ways to come at ith stair
-        dp[0] = 1;  
-        dp[1] = 1;
+        //constant space
+        int a = 1 , b = 1;
+        int c = 0;
         for(int i = 2 ; i <= n ; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return c;
     }
 }
