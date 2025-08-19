ceaa0932075d2fe35106013440ea962de281abeb
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-19 15:40:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (79.84%) - LeetSync

--- BinaryPlay/413-arithmetic-slices/arithmetic-slices.java
+++ BinaryPlay/413-arithmetic-slices/arithmetic-slices.java
@@ -4,32 +4,20 @@ class Solution {
         
         // length must be at least 3
         if(n < 3) return 0;
+ 
+        int sub = 0;  
+        int count = 0;
 
-        int i = 0 , j = 2;
-        int sub = 0; 
-        int diff = nums[1] - nums[0];
-        int currDiff = 0;
-        int cnt = 1;
-
-        while(j < n){
-            currDiff = (nums[j] - nums[j - 1]);
-            if(currDiff == diff){
-                if(j - i + 1 == 3){
-                    //equal to 3 length means new slice
-                    sub++;
-                    cnt = 1; //new slice found so reset it
-                }else if(j -  i + 1 > 3){
-                    //more than 3 length means same slice 
-                    sub += (cnt + 1);
-                    cnt++;
-                }
+        for(int i = 2 ; i < n ; i++){
+            int diff1 = nums[i] - nums[i - 1];
+            int diff2 = nums[i - 1] - nums[i - 2];
+            if(diff1 == diff2){
+                count++;
+                sub += count;
             }else{
-                //not equal
-                i = j - 1;
-                //new diff
-                diff = currDiff;
+                //intilise for new slice
+                count = 0;
             }
-            j++;
         }
         return sub;
 
