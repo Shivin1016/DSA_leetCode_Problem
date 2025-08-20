d511633262486ec64a915e9eef660b500d40bbac
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-20 14:50:44 +0530

    Time: 8 ms (18.33%) | Memory: 55.1 MB (62.24%) - LeetSync

--- BinaryPlay/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ BinaryPlay/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,41 +1,24 @@
 class Solution {
-    int m , n;
-    int[][] dp;
     public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        m = matrix.length;
-        n = matrix[0].length;
-
+        //bottom up appraoch
+        int[][] t = new int[m][n];
         int res = 0;
-        dp = new int[m + 1][n + 1];
-        for(int i = 0 ; i <= m ; i++) Arrays.fill(dp[i] , -1);
 
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){  
-                //agr cell me one hai tabhi recusrion maro
+            for(int j = 0 ; j < n ; j++){
                 if(matrix[i][j] == 1){
-                    res += solve(i , j , matrix);
+                    int up = (i - 1 < 0) ? 0 : t[i - 1][j];
+                    int left = (j - 1 < 0) ? 0 : t[i][j - 1];
+                    int diagonally_up = (i - 1 < 0 || j - 1 < 0) ? 0 : t[i - 1][j - 1];
+
+                    t[i][j] = 1 + Math.min(up , Math.min(left , diagonally_up));
+                    res += t[i][j];
                 }
             }
         }
-
         return res;
-    }
-    public int solve(int i , int j , int[][] matrix){
-
-        if(i >= m || j >= n) return 0;
-
-        if(matrix[i][j] == 0){
-            return 0; // is cell se possible hi nhi hai
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i , j + 1 , matrix);
-        int diagonally = solve(i + 1 , j + 1 , matrix);
-        int below = solve(i + 1 , j , matrix);
-
-        return dp[i][j] = 1 + Math.min(right , Math.min(diagonally , below));
-
     }
 }
