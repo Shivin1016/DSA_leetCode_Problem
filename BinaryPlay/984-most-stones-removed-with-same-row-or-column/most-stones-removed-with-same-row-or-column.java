cc12ccb327fe5fcf1bdeaaefa61523afc6f57307
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-21 15:21:49 +0530

    Time: 50 ms (8.08%) | Memory: 45.3 MB (36.71%) - LeetSync

---
+++ BinaryPlay/984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isSameGrp(int i , int j , int[][] stones){
+        if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+            return true;
+        }
+        return false;
+    }
+    public void dfs(int[][] stones , int i , boolean[] visited){
+
+        visited[i] = true;
+
+        for(int j = 0 ; j < stones.length ; j++){
+            if(!visited[j] && isSameGrp(i , j , stones)){ 
+                dfs(stones , j , visited);
+            }
+        }
+    }
+    public int removeStones(int[][] stones) {
+
+        int n = stones.length; // number of stones
+
+        int group = 0;
+        boolean[] visited = new boolean[n];
+
+        for(int i = 0 ; i < n ; i++){
+            if(!visited[i]){
+                dfs(stones , i , visited);
+                group++;
+            }
+        }
+ 
+        return  n - group;
+    }
+}
