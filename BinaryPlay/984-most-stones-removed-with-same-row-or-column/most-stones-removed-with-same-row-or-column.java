a156a9f90ee13bccee661b45541c37bcc5773d26
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-21 15:55:48 +0530

    Time: 57 ms (5.11%) | Memory: 45 MB (60.67%) - LeetSync

--- BinaryPlay/984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ BinaryPlay/984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -4,32 +4,67 @@ class Solution {
             return true;
         }
         return false;
+    } 
+    int[] parent ;
+    int[] rank; 
+    public int find(int i){
+        if(i == parent[i]) return i;
+
+        return parent[i] = find(parent[i]);
     }
-    public void dfs(int[][] stones , int i , boolean[] visited){
 
-        visited[i] = true;
+    public void union(int i , int j){
+        int i_parent = find(i);
+        int j_parent = find(j);
 
-        for(int j = 0 ; j < stones.length ; j++){
-            if(!visited[j] && isSameGrp(i , j , stones)){ 
-                dfs(stones , j , visited);
-            }
+        if(i_parent == j_parent) return ;
+
+        if(rank[i_parent] > rank[j_parent]){
+            parent[j_parent] = i_parent;
+        }
+        else if(rank[i_parent] < rank[j_parent]){
+            parent[i_parent] = j_parent;
+        }
+        else{
+            parent[i_parent] = j_parent;
+            rank[j_parent]++;
         }
     }
     public int removeStones(int[][] stones) {
 
         int n = stones.length; // number of stones
 
-        //using DFS
-        int group = 0;
-        boolean[] visited = new boolean[n];
+        //using DSU 
+        parent = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
+        }
+        rank = new int[n];
 
         for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){
-                dfs(stones , i , visited);
-                group++;
+
+            for(int j = 0 ; j < n ; j++){
+
+                if(i == j) continue;
+
+                if(isSameGrp(i , j , stones)){
+                    int i_parent = find(i);
+                    int j_parent = find(j);
+                    //not in same group then make group
+                    if(i_parent != j_parent){
+                        union(i , j);
+                    }
+                } 
             }
         }
+
+        //find total component
+        int components = 0; 
+        for(int i = 0 ; i < n ; i++){
+            //if parent is same as idx mtlb ye ek component ka boss hai 
+            if(find(i) == i) components++;
+        }
  
-        return  n - group;
+        return  n - components;
     }
 }
