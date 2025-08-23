10192717ec8f426ccc6ad2e10e4df9e649991b70
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-23 14:16:25 +0530

    Time: 208 ms (62.07%) | Memory: 63.1 MB (94.25%) - LeetSync

---
+++ BinaryPlay/3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long maxBalancedSubsequenceSum(int[] nums) {
+        int n = nums.length;
+
+        //stores val ->(nums[i] - i) and key ->sum ending at this (nums[i] - i)
+        TreeMap<Integer , Long> mp = new TreeMap<>(); 
+        long ans = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < n ; i++){
+
+            if(nums[i] <= 0){
+                ans = Math.max(nums[i] , ans);
+            }else{
+                int currKey = nums[i] - i;
+                //find key that is just less than or equal to ho is currkey se
+                long temp = nums[i];
+
+                if(mp.floorKey(currKey) != null){
+                    temp += mp.get(mp.floorKey(currKey));
+                }
+
+                //remove those keys which is greater than currKey and it's value is less then currKey value
+                while(mp.ceilingKey(currKey) != null && mp.get(mp.ceilingKey(currKey)) < temp){
+                    mp.remove(mp.ceilingKey(currKey));
+                }
+
+                mp.put(currKey , temp);
+
+                ans = Math.max(ans , temp);
+
+            }
+        }
+
+        return ans;
+
+    }
+}
