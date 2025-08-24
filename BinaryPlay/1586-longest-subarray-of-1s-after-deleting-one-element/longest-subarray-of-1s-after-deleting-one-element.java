eaf6dae8bb5d62c54595fd8036ad579064c1c070
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-24 14:18:22 +0530

    Time: 2 ms (98.24%) | Memory: 56.7 MB (75.93%) - LeetSync

--- BinaryPlay/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ BinaryPlay/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,23 +1,18 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        //sliding window 
-        int n = nums.length;
-        int i = 0 , j = 0;
+        //better sliding window approach
 
-        int result = 0 , zero_cnt = 0;
+        int n = nums.length;
+
+        int i = 0 , j = 0; 
+        int result =0;
+        int last_time_zero =-1;
         while(j < n){
             if(nums[j] == 0){
-                zero_cnt++;
+                i = last_time_zero + 1;
+                last_time_zero = j;
             }
-
-            while(i < n && zero_cnt > 1){
-                if(nums[i] == 0){
-                    zero_cnt--;
-                }
-                i++;
-            }
-            //we take length of (j - i) because we have to delete one element
-            result = Math.max(result , (j - i)); 
+            result = Math.max(result , j - i);
             j++;
         }
         return result;
