3a8b5120235e1d9baf7884918ef1df8835e4f6f3
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-26 15:15:34 +0530

    Time: 20 ms (72.89%) | Memory: 50.9 MB (67.69%) - LeetSync

--- BinaryPlay/1250-longest-common-subsequence/longest-common-subsequence.java
+++ BinaryPlay/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -7,18 +7,18 @@ class Solution {
         int[][] t = new int[n1 + 1][n2 + 1];
         //t[i][j] -->Longest common subsequence between s1 of length i and s2 of length j
 
-        for(int i = 1 ; i <= n1 ; i++){ 
+        for(int i = n1 - 1 ; i >= 0 ; i--){ 
 
-            for(int j = 1 ; j <= n2 ; j++){
+            for(int j = n2 - 1 ; j >= 0 ; j--){
 
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
-                    t[i][j] = 1 + t[i - 1][j - 1];
+                if(text1.charAt(i) == text2.charAt(j)){
+                    t[i][j] = 1 + t[i + 1][j + 1];
                 }
                 else{
-                    t[i][j] = Math.max(t[i - 1][j] , t[i][j - 1]);
+                    t[i][j] = Math.max(t[i + 1][j] , t[i][j + 1]);
                 }
             }
         }
-        return t[n1][n2];
+        return t[0][0];
     }
 }
