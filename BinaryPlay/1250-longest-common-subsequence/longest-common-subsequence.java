3bf781049baac5e54b3eda141905b2fe8dc506a4
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-26 14:54:41 +0530

    Time: 19 ms (87.09%) | Memory: 50.9 MB (67.69%) - LeetSync

--- BinaryPlay/1250-longest-common-subsequence/longest-common-subsequence.java
+++ BinaryPlay/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,28 +1,24 @@
 class Solution {
-    int n1 , n2 ;
-    int[][] t;
     public int longestCommonSubsequence(String text1, String text2) {
-        n1 = text1.length();
-        n2 = text2.length();
 
-        t = new int[1001][1001];
-        for(int[] row : t) Arrays.fill(row , -1);
+        int n1 = text1.length();
+        int n2 = text2.length();
 
-        return solve(0 , 0 , text1.toCharArray() , text2.toCharArray());
-    }
-    public int solve(int i , int j , char[] s1 , char[] s2){
+        int[][] t = new int[n1 + 1][n2 + 1];
+        //t[i][j] -->Longest common subsequence between s1 of length i and s2 of length j
 
-        if(i >= n1 || j >= n2){
-            return 0;
+        for(int i = 1 ; i <= n1 ; i++){ 
+
+            for(int j = 1 ; j <= n2 ; j++){
+
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                }
+                else{
+                    t[i][j] = Math.max(t[i - 1][j] , t[i][j - 1]);
+                }
+            }
         }
-
-        if(t[i][j] != -1) return t[i][j];
-
-        if(s1[i] == s2[j]){
-            return t[i][j] = 1 + solve(i + 1 , j + 1 , s1 , s2);
-        }
-        else{
-            return t[i][j] = Math.max(solve(i + 1 , j , s1 , s2) , solve(i , j + 1 , s1 , s2));
-        }
+        return t[n1][n2];
     }
 }
