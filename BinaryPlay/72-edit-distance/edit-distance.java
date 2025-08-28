8e9645c451f6411915d31031b9e50b69c2b01eb9
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-28 13:49:22 +0530

    Time: 5 ms (60.39%) | Memory: 45.3 MB (18.43%) - LeetSync

--- BinaryPlay/72-edit-distance/edit-distance.java
+++ BinaryPlay/72-edit-distance/edit-distance.java
@@ -1,47 +1,27 @@
 class Solution {
-    int m , n;
-    int[][] t;
+    int m , n; 
     public int minDistance(String word1, String word2) {
         m = word1.length();
         n = word2.length();
 
-        t = new int[501][501];
-        for(int[] r : t){
-            Arrays.fill(r , -1);
+        //bottom up
+        int[][] t = new int[m + 1][n + 1]; 
+        // t[i][j] -- > number of operation needed if s2 has length i and s2 has lenght j
+
+        for(int i = 0 ; i <= m ; i++){
+            for(int j = 0 ; j <= n ; j++){
+                if(i == 0 || j== 0){
+                    t[i][j] = i + j;
+                }
+                else if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    t[i][j] = t[i - 1][j - 1];
+                }else{
+                    t[i][j] = 1 + Math.min(t[i - 1][j - 1] , Math.min(t[i][j - 1] , t[i - 1][j]));
+                }
+            }
         }
 
-        return solve(m , n , word1.toCharArray() , word2.toCharArray());
+        return t[m][n];
     }
-
-    public int solve(int i , int j , char[] s1 , char[] s2){
-
-        if(i <= 0 && j <= 0){
-            return 0;
-        }
-
-        if(t[i][j] != -1) return t[i][j];
-
-        if(i > 0 && j == 0){
-            return t[i][j] = i;
-        }
-        if(i == 0 && j > 0){
-            return t[i][j] = j;
-        }
-
-        if(s1[i - 1] == s2[j - 1]){
-            return t[i][j] = solve(i - 1 , j - 1 , s1 , s2);
-        }
-
-        //insert
-        int insert = 1 + solve(i , j - 1 , s1 , s2);
-
-        //replcae
-        int replace = 1 + solve(i - 1 , j - 1 , s1 , s2);
-
-        // delete
-        int delete = 1 + solve(i - 1  , j , s1  , s2);
-
-        return t[i][j] = Math.min(insert , Math.min(replace , delete ));
-
-    }
+ 
 }
