edcf296f5bff2a1dcc18c2336365a48d1e0fae20
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-28 16:00:12 +0530

    Time: 46 ms (22.02%) | Memory: 56.6 MB (5.37%) - LeetSync

--- BinaryPlay/647-palindromic-substrings/palindromic-substrings.java
+++ BinaryPlay/647-palindromic-substrings/palindromic-substrings.java
@@ -1,35 +1,36 @@
 class Solution {
-    int[][] t;
     public int countSubstrings(String s) {
-
         int n = s.length();
 
+        //bottom up approach
         int count = 0;
-        t = new int[n + 1][n + 1];
-        for(int[] r : t){
-            Arrays.fill(r , -1);
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i ; j < n ; j++){
-                if(checkPlaindrom(i , j , s.toCharArray()) == 1){
-                    count++;
+        int[][] t = new int[1001][1001];
+        
+        for(int l = 1 ; l <= n ; l++){ //define length for substring
+            //iterate over each valid substring of length 'L'
+            for(int i = 0 ; i + l - 1 < n ; i++){
+                int j = i + l - 1; // valid j
+                if(i == j){
+                    //one length substring
+                    t[i][i] = 1;
+                }
+                else if(j == i + 1){
+                    //two length substring
+                    if(s.charAt(i) == s.charAt(j)){
+                        t[i][j] = 1;
+                    }
+                }else{
+                    //genric part length greater than 2
+                    //substring of length l -> if first char and last char equal then check the substring between them is plaindrom or not
+                    if(s.charAt(i) == s.charAt(j) && t[i + 1][j - 1] == 1){
+                        t[i][j] = 1;
+                    }
+                }
+                if(t[i][j] == 1){
+                    count++; // found plaindrom substring of length L 
                 }
             }
         }
-
         return count;
-    }
-
-    public int checkPlaindrom(int i , int j , char[] s){
-        if(i > j){
-            return 1;
-        }
-        if(t[i][j] != -1) return t[i][j];
-
-        if(s[i] == s[j]){
-            return t[i][j] = checkPlaindrom(i + 1 , j - 1 , s);
-        }
-        return t[i][j] = 0;
     }
 }
