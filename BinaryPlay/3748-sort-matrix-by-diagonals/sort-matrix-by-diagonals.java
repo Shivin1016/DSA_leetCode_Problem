ac1c75cb0b61ded59389dbcbda9e524ef2da1eac
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-28 12:33:49 +0530

    Time: 5 ms (69.27%) | Memory: 45.6 MB (78.44%) - LeetSync

--- BinaryPlay/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ BinaryPlay/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,42 +1,49 @@
 class Solution {
+    int m , n;
+    public List<Integer> sortDiagonal(int i , int j , int[][] grid){
+        List<Integer> temp = new ArrayList<>();
+
+        while(i < m && j < n){
+            temp.add(grid[i][j]);
+            i++;
+            j++;
+        }
+        Collections.sort(temp);
+        return temp;
+    }
     public int[][] sortMatrix(int[][] grid) {
 
-        int m = grid.length;
-        int n = grid[0].length;
-
-        Map<Integer , List<Integer>> mp = new HashMap<>();
+        m = grid.length;
+        n = grid[0].length;
+  
+        //bottom-left diagonal -->Dcreasing order
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                int key = i - j;
-                if(!mp.containsKey(key)){
-                    mp.put(key , new ArrayList<>());
-                }
-                mp.get(key).add(grid[i][j]);
-            }
-        }
+            int rowStart = i;
+            int colStart = 0;
 
-        for(int key : mp.keySet()){
-            List<Integer> ls = mp.get(key);
-            Collections.sort(ls);
-            if(key >= 0){
-                Collections.reverse(ls);
+            List<Integer> tempSort = sortDiagonal(rowStart , colStart , grid);
+            Collections.reverse(tempSort);
+            while(rowStart < m && colStart < n){
+                grid[rowStart][colStart] = tempSort.get(colStart);
+                rowStart++;
+                colStart++;
             }
-        }
+        } 
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                int key = i - j;
-                List<Integer> ls = mp.get(key);
-                grid[i][j] = ls.get(0);
-                ls.remove(0);
-                if(ls.size() == 0){
-                    mp.remove(key);
-                }
-                else{
-                    mp.put(key , ls);
-                }
+        //Top-right diagonal -->Increasing order
+        for(int j = 1 ; j < n ; j++){
+            int rowStart = 0;
+            int colStart = j;
+
+            List<Integer> tempSort = sortDiagonal(rowStart , colStart , grid);
+            int idx = 0;
+            while(rowStart < m && colStart < n){
+                grid[rowStart][colStart] = tempSort.get(idx++);
+                rowStart++;
+                colStart++;
             }
-        }
+        } 
+
 
         return grid;
     }
