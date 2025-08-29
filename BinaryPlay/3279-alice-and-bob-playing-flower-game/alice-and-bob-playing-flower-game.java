9095e4b27f92331d4b6348d9ba07d14730bfa63d
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-29 15:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (86.88%) - LeetSync

--- BinaryPlay/3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ BinaryPlay/3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,6 +1,12 @@
 class Solution {
-    public long flowerGame(int n, int m) {
-        long count = ((long)m * (long)n) / 2;
-        return count;
+    public long flowerGame(int n, int m) { 
+        //alice win when the count of x and y lane flower is odd
+        //count od comes when (x -> even and y -> odd) or (x ->odd and y -> even)
+
+        //now probability of x -> even and y -> odd is (n / 2) * (m + 1)/2
+        //or x ->odd and y -> even is (n + 1) /2 * (m / 2)
+        long p1 = ((long)(n) / 2) * ((long)(m + 1)/2);
+        long p2 = ((long)(n + 1) / 2) * ((long)(m)/2);
+        return p1 + p2;
     }
 }
