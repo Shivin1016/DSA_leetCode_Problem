4d63a38538c1b910cae83456ee845bc0c263f1b4
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-08-30 13:47:58 +0530

    Time: 36 ms (12.39%) | Memory: 45.4 MB (72.25%) - LeetSync

---
+++ BinaryPlay/2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+
+        int n = matrix.length;
+        int sum = n * (n + 1) / 2;
+
+        for(int i = 0 ; i < n ; i++){
+            Set<Integer> num1 = new HashSet<>();
+            Set<Integer> num2 = new HashSet<>();
+            for(int j = 0 ; j < n ; j++){
+                //col - wise
+                if(num1.contains(matrix[i][j])){
+                   return false;
+                }else{
+                    num1.add(matrix[i][j]);
+                }
+                //row wise
+                if(num2.contains(matrix[j][i])){
+                    return false;
+                }else{
+                    num2.add(matrix[j][i]);
+                }
+            } 
+        }  
+        return true;
+    }
+}
