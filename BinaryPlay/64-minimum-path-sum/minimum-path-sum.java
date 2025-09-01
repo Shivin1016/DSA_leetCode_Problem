b55a9a5f81329ba52b996712d2087fe12e99d57b
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-01 17:44:24 +0530

    Time: 1 ms (99.75%) | Memory: 47.4 MB (81.35%) - LeetSync

--- BinaryPlay/64-minimum-path-sum/minimum-path-sum.java
+++ BinaryPlay/64-minimum-path-sum/minimum-path-sum.java
@@ -1,29 +1,25 @@
 class Solution {
     int m , n;  
-    public int minPathSum(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;  
+    int[][] t ;
+    public int solve(int i , int j , int[][] grid){
 
-        int[][] t = new int[m][n];
-        //t[i][j] == mini path sum to reach (i)[j] from (0 , 0)
-        t[0][0] = grid[0][0];
+        if(i == m - 1 && j == n - 1) return t[i][j] = grid[i][j];
 
-        //fill first column
-        for(int i = 1 ; i < m ; i++){
-            t[i][0] = grid[i][0] + t[i - 1][0];
-        }
-        //fill first row
-        for(int j = 1 ; j < n ; j++){
-            t[0][j] = grid[0][j] + t[0][j - 1];
-        }
+        if(i >= m || j >= n) return Integer.MAX_VALUE;
 
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 1 ; j < n ; j++){ 
-                t[i][j] = grid[i][j] + Math.min(t[i - 1][j] , t[i][j - 1]); 
-            }
-        }
+        if(t[i][j] != -1) return t[i][j];
 
-        return t[m - 1][n - 1];
-
+        return t[i][j] = grid[i][j] + Math.min(solve(i , j + 1 , grid) , solve(i + 1 , j , grid));
     }
+    public int minPathSum(int[][] grid) {
+
+        m = grid.length;
+        n = grid[0].length; 
+
+        t = new int[m + 1][n + 1];
+        for(int[] r : t) Arrays.fill(r , -1);  
+
+        return solve(0 , 0 , grid);
+    }
+
 }
