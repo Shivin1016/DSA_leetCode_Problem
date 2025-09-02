7599c7498b6cfec010e933ed4077d4eb1cb4f286
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-02 18:28:32 +0530

    Time: 2 ms (68.55%) | Memory: 46 MB (5.74%) - LeetSync

--- BinaryPlay/174-dungeon-game/dungeon-game.java
+++ BinaryPlay/174-dungeon-game/dungeon-game.java
@@ -1,36 +1,31 @@
 class Solution {
-    int m ,n ;
-    int[][] t = new int[201][201];
-    public int solve(int i , int j , int[][] grid){
-
-        if(i >= m || j >= n){  
-            return Integer.MAX_VALUE;
-        }   
-
-        if(t[i][j] != -1) return t[i][j];
-
-        if(i == m - 1 && j == n - 1){
-            //base case
-            if(grid[i][j] <= 0){
-                return t[i][j] = Math.abs(grid[i][j]) + 1;
-            }
-            return 1; // already have positive power so minimum 1 required
-        } 
- 
-
-        int requireMnt = Math.min(solve(i , j + 1 , grid) , solve(i + 1 , j , grid)) - grid[i][j];
-        //if ans less then 0 mtln reuiremnt se jada hi hai to one bhej do
-        int ans = requireMnt <= 0 ? 1 : requireMnt;
-       
-        return t[i][j] = ans;
-    }
+    int m ,n ;  
     public int calculateMinimumHP(int[][] dungeon) {
         m = dungeon.length;
-        n = dungeon[0].length;  
+        n = dungeon[0].length;   
 
-        for(int[] r : t) Arrays.fill(r , -1);
+        int[][] t = new int[m][n];
 
-        return solve(0 ,0 , dungeon);
+        for(int i = m - 1 ; i >= 0 ; i--){
+            for(int j = n - 1 ; j >= 0 ; j--){
+                if(i == m - 1 && j == n - 1){
+                    if(dungeon[i][j] > 0){
+                        t[i][j] = 1;
+                    }else{
+                        t[i][j] = Math.abs(dungeon[i][j]) + 1;
+                    }
+                }
+                else{
+                    int right = (j + 1) >= n ? Integer.MAX_VALUE : t[i][j + 1];
+                    int down = (i + 1 ) >= m ? Integer.MAX_VALUE : t[i + 1][j];
+
+                    int res = Math.min(right , down) - dungeon[i][j];
+                    t[i][j] = res > 0 ? res : 1;
+                }
+            }
+        }
+
+        return t[0][0]; 
     }
 
 
