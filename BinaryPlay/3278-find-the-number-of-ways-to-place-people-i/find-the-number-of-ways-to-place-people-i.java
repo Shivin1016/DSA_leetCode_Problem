91e2df6dee48cba23352348f411edc118ae20115
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-03 14:39:58 +0530

    Time: 5 ms (100.00%) | Memory: 44.9 MB (15.79%) - LeetSync

--- BinaryPlay/3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
+++ BinaryPlay/3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -2,40 +2,41 @@ class Solution {
     public int numberOfPairs(int[][] points) {
         int n = points.length;
 
-        // Arrays.sort(points , (a , b) -> a[0] - b[0]);
+        Arrays.sort(points , new Comparator<int[]>(){
+           
+            public int compare(int[] a , int[] b){
+                if(a[0] == b[0]){
+                    return b[1] - a[1];
+                }
+                return a[0] - b[0];
+            }
+        });
 
-        int count = 0;
+        int ans = 0;
 
         for(int i = 0 ; i < n ; i++){
 
-            int[] p1 = points[i];
+            int x1 = points[i][0];
+            int y1 = points[i][1];
 
-            for(int j = 0 ; j < n ; j++){
+            int maxY = Integer.MIN_VALUE;
 
-                int[] p2 = points[j]; 
-                if(i == j || p1[0] > p2[0] || p1[1] < p2[1]){
-                    continue;
-                } 
-                boolean isInside = true;
-                for(int k = 0 ; k < n ; k++){
+            for(int j = i + 1 ; j < n ; j++){
 
-                    if(k == i || k == j) continue;
+                int x2 = points[j][0];
+                int y2 = points[j][1];   
 
-                    int x = points[k][0] ;
-                    int y = points[k][1];
-                    // inside rectangle 
-                    if(p1[0] <= x && p2[0] >= x && p1[1] >= y && p2[1] <= y){
-                        isInside = false;
-                        break;
-                    }
+                if(y2 > y1){
+                    continue ; // upper chala gaya  // invalid
                 }
-                
-                if(isInside){
-                    count++;
+
+                if(y2 > maxY){
+                    ans++;
+                    maxY = y2;
                 }
             }
         }
 
-        return count;
+        return ans;
     }
 }
