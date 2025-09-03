d6729037a920e33503913d976d72dc75a8ec221e
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-03 20:00:39 +0530

    Time: 16 ms (47.99%) | Memory: 198.4 MB (42.78%) - LeetSync

--- BinaryPlay/3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
+++ BinaryPlay/3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
@@ -1,71 +1,41 @@
-class Solution { 
-    int[][] t1 ;
-    int[][] t2;
-    public int solve1(int i , int j , int[][] fruits , int n ){
-        if(i >= n || j >= n || j < 0  ){ 
-            return 0;
-        }
-
-        if(t1[i][j] != -1) return t1[i][j];
-
-        if(i == n - 1 && j == n - 1){
-            return t1[i][j] = 0;// fruits already taken by child1
-        }
-
-        // if it goes opposite ke diagonal me y aon diagonal then return
-        if(i > j || i == j){
-            return t1[i][j] =  0;
-        } 
-
-        int bottom_left = fruits[i][j] +  solve1(i + 1 , j - 1 , fruits , n );
-        int bottom_down = fruits[i][j] + solve1(i + 1 , j , fruits , n );
-        int bottom_right = fruits[i][j] + solve1(i + 1 , j + 1 , fruits , n);
-
-        return t1[i][j] = Math.max(bottom_left , Math.max(bottom_down , bottom_right));
-    }
-    public int solve2(int i , int j , int[][] fruits , int n ){
-
-        if(i >= n || j >= n || i < 0 ){
-        // System.out.println("Me=> " + moves);
-            return 0;
-        }
-        if(t2[i][j] != -1) return t2[i][j];
-
-        if(i == n - 1 && j == n - 1){
-            return t2[i][j] = 0; // fruits already taken by child1
-        } 
-
-        if(i < j || i == j) return t2[i][j] = 0 ; // on diagonal or opposite diagonal 
-
-        int top_right = fruits[i][j] +  solve2(i - 1 , j + 1 , fruits  , n    );
-        int right = fruits[i][j] + solve2(i , j + 1 , fruits , n   );
-        int bottom_right = fruits[i][j] + solve2(i + 1 , j + 1 , fruits , n    );
-
-        return t2[i][j] = Math.max(top_right , Math.max(right , bottom_right));
-    }
-
+class Solution {
     public int maxCollectedFruits(int[][] fruits) {
-        int N = fruits.length; 
-        //for child1 -->it can only go diagonal on elements otherwise it can't react to n - 1 , n- 1 cell because of moves will go greater than n - 1
+        int n = fruits.length;
 
-        int child1_collection1 = 0;
-        for(int i = 0 ; i < N ; i++){
-            child1_collection1 += fruits[i][i]; //aceess diagonals
-            fruits[i][i] = 0 ;// nullify it , because it taken by child1
+        int[][] t = new int[n][n];
+
+        int result = 0;
+        for(int i = 0 ; i < n ; i++){
+            result += fruits[i][i];
         }
 
-        t1 = new int[N + 1][N + 1];
-        for(int[]r : t1) Arrays.fill(r , -1);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i < j && (i + j) < n - 1){
+                    t[i][j] = 0;
+                }else if(i > j && (i + j) < n - 1){
+                    t[i][j] = 0;
+                }else{
+                    t[i][j] = fruits[i][j];
+                }
 
-        t2 = new int[N + 1][N + 1];
-        for(int[]r : t2) Arrays.fill(r , -1);
+            }
+        }
 
-        int child1_collection2 = solve1(0 , N - 1 , fruits ,N ); //moves = 0 in starting
+        //for child2
+        for(int i = 1 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                t[i][j] += Math.max(t[i - 1][j] , Math.max(t[i - 1][j - 1] , ((j + 1) < n) ? t[i- 1][j + 1] : 0));
+            }
+        }
 
-        int child1_collection3 = solve2(N - 1 , 0 , fruits,N); 
+        //for child2
+        for(int j = 1 ; j < n ; j++){
+            for(int i = j + 1 ; i < n ; i++){
+                t[i][j] += Math.max(t[i - 1][j - 1] , Math.max(t[i][j - 1] , ((i + 1) < n) ? t[i + 1][j - 1] : 0));
+            }
+        }
 
-        System.out.println(child1_collection2 + " " + child1_collection3);
-
-        return child1_collection1 + child1_collection2 + child1_collection3;
+        return result + t[n - 1][n - 2] + t[n - 2][n - 1];
     }
 }
