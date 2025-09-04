4b4ee29de310f7016c98dcc4041d38d2a7768f5b
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-04 13:57:43 +0530

    Time: 7 ms (56.80%) | Memory: 45.4 MB (52.28%) - LeetSync

--- BinaryPlay/967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ BinaryPlay/967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,42 +1,9 @@
-class Solution {
-    int n ;
-    int[][] t;
-    public int solve(int i , int j , int[][] matrix){  
-
-        if(i == n - 1){
-            //reached last row 
-            return t[i][j] = matrix[i][j]; 
-        } 
-
-        if(t[i][j] != -1) return t[i][j];
-
-        int min_sum = Integer.MAX_VALUE;
-
-        if(i + 1 < n && j + 1 < n){
-            min_sum = Math.min(min_sum , matrix[i][j] + solve(i + 1 , j + 1 , matrix));
-        }
-        if(i + 1 < n && j < n){
-            min_sum = Math.min(min_sum , matrix[i][j] + solve(i + 1 , j , matrix));
-        }
-        if(i + 1 < n && j - 1 >= 0){
-            min_sum = Math.min(min_sum , matrix[i][j] + solve(i + 1 , j - 1 , matrix));
-        } 
-
-        return t[i][j] = min_sum;
-
-        // return t[i][j] = matrix[i][j] + Math.min(bottom_right , Math.min(bottom_down , bottom_left));
-
-    }
+class Solution { 
     public int minFallingPathSum(int[][] matrix) {
 
-        n = matrix.length;
+        int n = matrix.length;
 
-        int min_falling_path_sum = Integer.MAX_VALUE;
-
-        t = new int[n + 1][n + 1];
-        for(int[] r : t){
-            Arrays.fill(r , -1);
-        } 
+        int min_falling_path_sum = Integer.MAX_VALUE; 
 
         int[][] t = new int[n][n];
         for(int col = 0 ; col < n ; col++){
