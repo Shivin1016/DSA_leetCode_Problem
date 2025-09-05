cdef87593da681d9a249381c318e1327786f6868
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-05 15:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (44.62%) - LeetSync

--- BinaryPlay/2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
+++ BinaryPlay/2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -1,16 +1,16 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
-        long t = 1;
-        while(true){
-            long val = num1 - (long)(t * num2);
+        
+        for(long t = 1 ; t <= 36 ; t++){
+            long val = (long)num1 - (long)(t * num2);
             if(val < 0){
                 return -1;
             }
             long min_bit = Long.bitCount(val);
             if(min_bit <= t && t <= val){
                 return (int)t;
-            }
-            t++;
+            } 
         } 
+        return -1;
     }
 }
