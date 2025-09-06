037f71d0c718cf1f84e4ad83f368ad18518ef9b6
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-06 16:27:25 +0530

    Time: 42 ms (12.56%) | Memory: 52.4 MB (5.64%) - LeetSync

---
+++ BinaryPlay/1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,42 @@
+class Solution {
+    int n;
+    public boolean isDuplicate(String st){
+        int[] frq = new int[26];
+        // System.out.println(st);
+        for(char ch : st.toCharArray()){
+            frq[ch - 'a']++;
+            if(frq[ch - 'a'] > 1) return true; //find duplicates
+        }
+        return false;
+    }
+    Map<String , Integer> mp = new HashMap<>();
+    public int solve(int i , List<String> arr , String temp){
+        if(i >= n){
+            return temp.length();
+        }
+        String key = i + "_" + temp;
+        if(mp.containsKey(key)){
+            return mp.get(key);
+        }
+
+        //include 
+        int include = 0 , exclude = 0;
+        if(isDuplicate(temp + arr.get(i))){
+            //exlude in include path
+            include = solve(i + 1 , arr , temp); 
+        }else{
+            //not duplicate then include
+            include = solve(i + 1 , arr , temp + arr.get(i));
+            //exclude part
+            exclude = solve(i + 1 , arr , temp); 
+        }
+        key = i + "_" + temp;
+        mp.put(key , Math.max(include , exclude));
+        return Math.max(include , exclude);
+    }
+    public int maxLength(List<String> arr) {
+        n = arr.size();
+
+        return solve(0 , arr , "");
+    }
+}
