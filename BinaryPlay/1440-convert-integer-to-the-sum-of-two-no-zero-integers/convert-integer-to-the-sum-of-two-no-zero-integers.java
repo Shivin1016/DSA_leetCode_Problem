867b0b8baea6c8ce958f22313a6b8101eb0823f2
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-08 13:39:16 +0530

    Time: 1 ms (36.38%) | Memory: 41.4 MB (58.05%) - LeetSync

--- BinaryPlay/1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ BinaryPlay/1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -2,19 +2,19 @@ class Solution {
     public int[] getNoZeroIntegers(int n) {
         int a = 1; 
         while(true){
-            int b = n - a;
-            if(!isbContainsZero(b) && !isbContainsZero(a)){
+            int b = n - a; 
+            if(!Integer.toString(b).contains("0") && !Integer.toString(a).contains("0")){
                 return new int[]{b , a};
             }
             a++;
         }
         // return new int[]{};
     }
-    public boolean isbContainsZero(int b){
-        while(b != 0){
-            if(b % 10 == 0) return true;
-            b /= 10;
-        }
-        return false;
-    }
+    // public boolean isbContainsZero(int b){
+    //     while(b != 0){
+    //         if(b % 10 == 0) return true;
+    //         b /= 10;
+    //     }
+    //     return false;
+    // }
 }
