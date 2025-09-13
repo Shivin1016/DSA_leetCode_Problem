c83a94313df46465fbeb078f9a54593a299a95d1
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-13 13:32:23 +0530

    Time: 26 ms (71.28%) | Memory: 99.9 MB (97.52%) - LeetSync

--- BinaryPlay/2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.java
+++ BinaryPlay/2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.java
@@ -1,12 +1,13 @@
 class Solution {
-    public void dfs(List<List<int[]>> adj , int node , int minScore , boolean[] visited){
+    int minScore;
+    public void dfs(List<List<int[]>> adj , int node , boolean[] visited){
         visited[node] = true;
         for(int[] v : adj.get(node)){
             int u = v[0];
             int dist = v[1];
             minScore = Math.min(minScore , dist);
             if(!visited[u]){
-                dfs(adj , u , minScore , visited);
+                dfs(adj , u , visited);
             }
         }
     }
@@ -27,31 +28,31 @@ class Solution {
             adj.get(v).add(new int[]{u , dist});
         }
  
-        int minScore = Integer.MAX_VALUE;
+        minScore = Integer.MAX_VALUE;
         boolean[] visited = new boolean[n + 1];
 
         //using bfs
-        var que = new LinkedList<Integer>();
-        que.add(1);
-        visited[1] = true;
-        while(!que.isEmpty()){
+        // var que = new LinkedList<Integer>();
+        // que.add(1);
+        // visited[1] = true;
+        // while(!que.isEmpty()){
 
-            int u = que.pop();  
+        //     int u = que.pop();  
 
-            for(int[] nodeDist : adj.get(u)){
-                int v = nodeDist[0];
-                int dist = nodeDist[1];  
-                minScore = Math.min(minScore , dist);
-                if(!visited[v]){
-                    que.add(v); 
-                    visited[v] = true; 
-                }
-            }
-        }
+        //     for(int[] nodeDist : adj.get(u)){
+        //         int v = nodeDist[0];
+        //         int dist = nodeDist[1];  
+        //         minScore = Math.min(minScore , dist);
+        //         if(!visited[v]){
+        //             que.add(v); 
+        //             visited[v] = true; 
+        //         }
+        //     }
+        // }
         
 
         //using dfs 
-        // dfs(adj , 1 , minScore , visited); 
+        dfs(adj , 1 , visited); 
 
         return minScore;
     }
