492994d675c4fd2c47e6540ae1a55ce8b958db01
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-18 17:11:35 +0530

    Time: 41 ms (53.34%) | Memory: 113.1 MB (33.43%) - LeetSync

--- BinaryPlay/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ BinaryPlay/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,44 +1,21 @@
 class Solution {
-    int[] parent;
-    int[] rank;
-
     int size = 0;
-    public void DFS(List<List<Integer>> adj , int u , boolean[] visited){
-        if(visited[u] == true){
-            return ;
-        }
-        visited[u] = true; 
-        size++;
-        for(int v : adj.get(u)){
-            if(!visited[v]){
-                DFS(adj , v , visited); 
-            }
-        } 
-    }
-    public void BFS(List<List<Integer>> adj , int u , Queue<Integer> que, boolean[] visited){
-        que.offer(u);
+    public void dfs(List<List<Integer>> adj , int u , boolean[] visited){
         visited[u] = true;
         size++;
 
-        while(!que.isEmpty()){
-            int node = que.poll();
-            for(int v : adj.get(node)){
-                if(!visited[v] == true){
-                    que.offer(v);
-                    visited[v] = true;
-                    size++;
-                }
+        for(int v : adj.get(u)){
+            if(!visited[v]){
+                dfs(adj , v , visited);
             }
         }
     }
-
-    public long countPairs(int n, int[][] edges) { 
-
-        //using DFS or BFS
+    public long countPairs(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             adj.add(new ArrayList<>());
         }
+
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
@@ -46,98 +23,22 @@ class Solution {
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        boolean[] visited = new boolean[n]; 
-        Queue<Integer> que = new LinkedList<>();
 
-        long unreachablePair = 0;
+        boolean[] visited = new boolean[n];
         long totalNode = n;
+        long unreachable  = 0;
+
         for(int i = 0 ; i < n ; i++){
-            if(!visited[i]){ 
-                size = 0 ;
-                //using DFS
-                // DFS(adj , i , visited); 
-                //using BFS
-                BFS(adj , i , que , visited);
-                //remaingNodes by which pairs are formed
-                long remainingNode = totalNode - size;
-                unreachablePair += (size * remainingNode);
-                //remove current component nodes
-                totalNode -= size;
+            if(!visited[i]){
+                size = 0;
+
+                dfs(adj , i , visited);
+
+                long numberOfNodes = size;
+                unreachable += (numberOfNodes) * (totalNode - numberOfNodes);
+                totalNode -= numberOfNodes;
             }
         }
-
-        return unreachablePair;
-
-        
-
-        // Using DSU 
-        /*
-        parent = new int[n];
-        rank = new int[n]; 
-
-        for(int i = 0 ; i < n ; i++){
-            parent[i] = i;
-        }
-
-        for(int[] edge : edges){
-
-            int u = edge[0];
-            int v = edge[1];
-
-            int parent_u = find(u);
-            int parent_v = find(v);
-            //make components --> if parent not same then make union
-            if(parent_u != parent_v){
-                union(u , v);
-            }  
-        }
-
-        long totalNode = n;
-        //stores component size
-        Map<Integer , Integer> mp = new HashMap<>();
-        for(int u = 0 ; u < n ; u++){  
-            int parent = find(u);
-            mp.put(parent , mp.getOrDefault(parent , 0) + 1);
-        }
-
-        long unreachablePair = 0;
-        for(int val : mp.values()){
-            //remaingNodes by which pairs are formed
-            long remainingNode = totalNode - val;
-            unreachablePair += (val * remainingNode);
-            //remove current component nodes
-            totalNode -= val;
-        } 
-
-        return unreachablePair;
-        */
-    }
-
-    public void union(int u , int v){
-        int parent_u = find(u);
-        int parent_v = find(v);
-
-        if(parent_u == parent_v){
-            return;
-        }
-
-        if(rank[parent_u] > rank[parent_v]){
-            parent[parent_v] = parent_u;
-        } 
-        else if(rank[parent_u] < rank[parent_v]){
-            parent[parent_u] = parent_v;
-        }
-        else{
-            //same case --> make any of them and increase rank 
-            parent[parent_u] = parent_v;
-            rank[parent_v]++;
-        }
-    }
-
-    public int find(int u){
-        if(parent[u] == u){
-            return u;
-        } 
-        return parent[u] = find(parent[u]);
+        return unreachable; 
     }
 }
