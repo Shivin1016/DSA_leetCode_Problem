abe9177c46a74bd345eaf0332219ebddcf40518f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-23 16:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (32.01%) - LeetSync

--- BinaryPlay/165-compare-version-numbers/compare-version-numbers.java
+++ BinaryPlay/165-compare-version-numbers/compare-version-numbers.java
@@ -1,40 +1,32 @@
 class Solution {
-    public int compareVersion(String version1, String version2) {
-         
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");    
+    public int compareVersion(String version1, String version2) {    
 
-        int n1 = v1.length;
-        int n2 = v2.length;
+        int n1 = version1.length();
+        int n2 = version2.length();
  
 
         int i = 0 , j = 0; 
 
-        while(i < n1 && j < n2){
-            int num1 = Integer.parseInt(v1[i]);
-            int num2 = Integer.parseInt(v2[j]);
-            // System.out.println(num1 + " " + num2);
-            if(num1 < num2){
-                return -1;
-            }
-            else if(num1 > num2){
-                return 1;
-            }
-            i++;
-            j++;
-        }   
+        while(i < n1 || j < n2){ 
+            long num1 = 0 , num2  = 0;
 
-        while(i < n1){
-            int num1 = Integer.parseInt(v1[i]);
-            if(num1 > 0) return 1;
-            i++;
-        }
+            while(i < n1 && version1.charAt(i) != '.'){
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
 
-        while(j < n2){
-            int num2 = Integer.parseInt(v2[j]);
-            if(num2 > 0) return -1;
-            j++;
-        }
+            while(j < n2 && version2.charAt(j) != '.'){
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
+            if(num1 < num2) return -1;
+            if(num1 > num2) return 1;
+
+            if(i < n1 && version1.charAt(i) == '.') i++;
+            if(j < n2 && version2.charAt(j) == '.') j++;
+
+        }    
 
         return 0;
     }
