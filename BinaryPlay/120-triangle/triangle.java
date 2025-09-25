14cdbb7a4b9527e42914285a07ab7edf9fc59d51
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-25 15:59:38 +0530

    Time: 10 ms (6.00%) | Memory: 45.9 MB (7.00%) - LeetSync

--- BinaryPlay/120-triangle/triangle.java
+++ BinaryPlay/120-triangle/triangle.java
@@ -1,18 +1,20 @@
 class Solution { 
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
+        int n = triangle.size(); 
 
-        List<Integer> t = triangle.get(n - 1); //stors the last row 
-        //t.get(c) --> stores minimum path sum from c to bottom
-
-        for(int r = n - 2 ; r >= 0 ; r--){ 
+        for(int r = 1 ; r < n ; r++){
             for(int c = 0 ; c <= r ; c++){
-                int val = triangle.get(r).get(c) + Math.min(t.get(c) , t.get(c + 1));
-                t.set(c , val);
+                int size = triangle.get(r - 1).size();
+                int rowLeft = triangle.get(r - 1).get(Math.max(c - 1 , 0));
+                int rowRight = triangle.get(r - 1).get(Math.min(c , size - 1));
+                int val = triangle.get(r).get(c) + Math.min(rowLeft , rowRight);
+
+                triangle.get(r).set(c , val);
             }
         }
 
-        return t.get(0); //minimum path sum from top to bottom
+        Collections.sort(triangle.get(n - 1));
+        return triangle.get(n - 1).get(0);
     
     }
 }
