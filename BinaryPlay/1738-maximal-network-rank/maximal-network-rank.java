1d0aa0c8ca48d233eb180c3ea6f025d27c27c2d7
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-29 17:32:53 +0530

    Time: 4 ms (98.27%) | Memory: 46 MB (30.19%) - LeetSync

--- BinaryPlay/1738-maximal-network-rank/maximal-network-rank.java
+++ BinaryPlay/1738-maximal-network-rank/maximal-network-rank.java
@@ -1,32 +1,34 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
 
-        List<TreeSet<Integer>> adj = new ArrayList<>();
-
-        for(int i = 0 ; i < n ; i++){
-            adj.add(new TreeSet<>());
-        }
+        int[] indegree = new int[n];
+        boolean[][] connected = new boolean[n][n];
 
         for(int[] road : roads){
-            adj.get(road[0]).add(road[1]);
-            adj.get(road[1]).add(road[0]);
+            int u = road[0];
+            int v = road[1];
+
+            indegree[u]++;
+            indegree[v]++; 
+            connected[u][v] = true;
+            connected[v][u] = true;
         }
 
         int maxNetworkRank = Integer.MIN_VALUE;
         for(int i = 0 ; i < n ; i++){
 
             //ith node rank --> kitni nodes se connected hai ye
-            int i_rank = adj.get(i).size();
+            int i_rank = indegree[i];
 
             for(int j = i + 1 ; j < n ; j++){
 
                 //jth node rank --> kitni nodes se connected hai ye
-                int j_rank = adj.get(j).size(); 
+                int j_rank = indegree[j];
 
                 //dono ki ranks
                 int totalRank = i_rank + j_rank;
 
-                if(adj.get(i).contains(j)){
+                if(connected[i][j] == true){
                     //connected hai to edge ek count hogi
                     totalRank -= 1;
                 }
