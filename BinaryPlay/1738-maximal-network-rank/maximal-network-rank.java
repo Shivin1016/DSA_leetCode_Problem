41638a041239068de30963dd4a6183174830ca1d
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-09-29 17:28:32 +0530

    Time: 25 ms (42.50%) | Memory: 45.8 MB (50.19%) - LeetSync

--- BinaryPlay/1738-maximal-network-rank/maximal-network-rank.java
+++ BinaryPlay/1738-maximal-network-rank/maximal-network-rank.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
 
-        List<Set<Integer>> adj = new ArrayList<>();
+        List<TreeSet<Integer>> adj = new ArrayList<>();
 
         for(int i = 0 ; i < n ; i++){
-            adj.add(new HashSet<>());
+            adj.add(new TreeSet<>());
         }
 
         for(int[] road : roads){
@@ -14,14 +14,23 @@ class Solution {
 
         int maxNetworkRank = Integer.MIN_VALUE;
         for(int i = 0 ; i < n ; i++){
+
+            //ith node rank --> kitni nodes se connected hai ye
             int i_rank = adj.get(i).size();
+
             for(int j = i + 1 ; j < n ; j++){
-                int j_rank = adj.get(j).size();
+
+                //jth node rank --> kitni nodes se connected hai ye
+                int j_rank = adj.get(j).size(); 
+
+                //dono ki ranks
                 int totalRank = i_rank + j_rank;
+
                 if(adj.get(i).contains(j)){
                     //connected hai to edge ek count hogi
                     totalRank -= 1;
                 }
+                //max rank in whole graph
                 maxNetworkRank = Math.max(maxNetworkRank , totalRank);
             }
         }
