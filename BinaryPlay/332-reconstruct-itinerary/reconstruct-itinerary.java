5ecb8ec14ce093d00a6c389c5d4a8b342b51c4c5
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-10-08 19:17:53 +0530

    Time: 5 ms (99.88%) | Memory: 45.5 MB (65.81%) - LeetSync

--- BinaryPlay/332-reconstruct-itinerary/reconstruct-itinerary.java
+++ BinaryPlay/332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,31 +1,24 @@
-class Solution {
-    List<String> ans = new ArrayList<>();
+class Solution { 
 
-    public void DFS(Map<String , PriorityQueue<String>> adj , String u){
-        PriorityQueue<String> pq = adj.get(u);
-         
+    public void dfs(String fromCity , Map<String , PriorityQueue<String>> adj , List<String> path){
+        PriorityQueue<String> pq = adj.get(fromCity);
         while(pq != null && !pq.isEmpty()){
-            //remove particular v  
-            String v = pq.poll();
-            DFS(adj , v);
-        } 
-        
-        ans.add(u);
+            String toCity = pq.poll();
+            dfs(toCity , adj , path);
+        }
+        path.addFirst(fromCity);
     }
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String , PriorityQueue<String>> adj = new HashMap<>();
+        Map<String , PriorityQueue<String>> adj = new HashMap<>(); 
 
-        int n = tickets.size();
-        for(int i = 0 ; i < n ; i++){
-            List<String> ticket = tickets.get(i); 
-            String from = ticket.get(0);
-            String to = ticket.get(1);
-            adj.computeIfAbsent(from , k -> new PriorityQueue<>()).add(to) ;
-
-        }
- 
-        DFS(adj , "JFK");
-        Collections.reverse(ans);
-        return ans;
+        for(List<String> ticket : tickets){
+            String fromCity = ticket.get(0);
+            String toCity = ticket.get(1); 
+            adj.computeIfAbsent(fromCity , k -> new PriorityQueue<>()).add(toCity);
+        } 
+        
+        List<String> path = new ArrayList<>();
+        dfs("JFK" , adj , path);
+        return path;
     }
 }
