18cde6353394db0d3c74735edb458c149fe8cd48
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-10-15 15:42:54 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (97.06%) - LeetSync

--- BinaryPlay/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
+++ BinaryPlay/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -1,23 +1,23 @@
 class Solution {
-    public boolean isIncreasing(List<Integer> nums , int i , int n){
-        while(i < n - 1){
-            if(nums.get(i) >= nums.get(i + 1)) return false;
-            i++;
-        }
-        return true;
-    }
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
         int n = nums.size();
 
-        for(int start = 0 ; start + 2 * k <= n ; start++){
-            boolean first = isIncreasing(nums , start , start + k);
-            if(first == true){
-                boolean second = isIncreasing(nums , start + k , start + k + k);
-                if(first == true && second == true){
-                    return true;
-                }
+        int prevLen = 0;
+        int currLen = 1;
+
+        int maxResult = 0;
+
+        for(int i = 1 ; i < n ; i++){
+            if(nums.get(i) > nums.get(i - 1)){
+                currLen++;
+            }
+            else{ // increasing order breaks
+                prevLen = currLen;
+                currLen = 1;
             }
 
+            if(currLen / 2 >= k) return true;
+            else if(Math.min(currLen , prevLen) >= k) return true;
         }
         return false;
     }
