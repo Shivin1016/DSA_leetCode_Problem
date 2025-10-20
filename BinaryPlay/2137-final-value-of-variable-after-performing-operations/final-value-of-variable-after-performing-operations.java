e386883d74b80e8d7b22dd4bfc954c203ac2cc20
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-10-20 11:39:50 +0530

    Time: 1 ms (74.53%) | Memory: 43 MB (24.22%) - LeetSync

--- BinaryPlay/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++ BinaryPlay/2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
         for(String op : operations){
-            if(op.contains("++")){
+            if(op.equals("++X") || op.equals("X++")){
                 x++;
             }else{
                 x--;
