f87b5b9f5d455018209da8d29667664a553078a3
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-02 16:02:27 +0530

    Time: 38 ms (18.98%) | Memory: 150.5 MB (5.11%) - LeetSync

--- BinaryPlay/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ BinaryPlay/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,7 +1,30 @@
 class Solution {
+    int total , N , M;
+    int[][] visited ;
+    public void dfs(int i , int j , String dir){
+        if(i < 0 || i >= M || j < 0 || j >= N) return;
+
+        if(visited[i][j] == 2 || visited[i][j] == 3 ) return;
+
+        if(visited[i][j] == 0){
+            visited[i][j] = 1;
+            total--;  
+        }
+        if(dir.equals("left")){
+            dfs(i , j - 1 , dir);
+        }else if(dir.equals("right")){
+            dfs(i , j + 1 , dir);
+        }else if(dir.equals("down")){
+            dfs(i + 1 , j , dir);
+        }else if(dir.equals("up")){
+            dfs(i - 1 , j , dir);
+        } 
+    }
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] visited = new int[m][n];
-        int total = m * n;
+        N = n ;
+        M = m ;
+        visited = new int[m][n];
+        total = m * n;
         //guards --> 3
         for(int i = 0 ; i < guards.length ; i++){
             visited[guards[i][0]][guards[i][1]] = 3;
@@ -18,37 +41,13 @@ class Solution {
             int c = guards[i][1];
 
             //for left
-            for(int j = c - 1 ; j >= 0 ; j--){
-                if(visited[r][j] == 2 || visited[r][j] == 3) break;
-                if(visited[r][j] == 0){
-                    visited[r][j] = 1;
-                    total--;
-                }
-            }
+            dfs(r , c - 1 , "left"); 
             //for right
-            for(int j = c + 1 ; j < n ; j++){
-                if(visited[r][j] == 2 || visited[r][j] == 3) break;
-                if(visited[r][j] == 0){
-                    visited[r][j] = 1;
-                    total--;
-                }
-            }
+            dfs(r , c + 1 , "right");
             //for down
-            for(int j = r + 1 ; j < m ; j++){
-                if(visited[j][c] == 2 || visited[j][c] == 3) break;
-                if(visited[j][c] == 0){
-                    visited[j][c] = 1;
-                    total--;
-                }
-            }
+            dfs(r + 1 , c , "down");
             //for up
-            for(int j = r - 1 ; j >= 0 ; j--){
-                if(visited[j][c] == 2 || visited[j][c] == 3) break;
-                if(visited[j][c] == 0){
-                    visited[j][c] = 1;
-                    total--;
-                }
-            }
+            dfs(r - 1 , c , "up");
         }
 
         return total;
