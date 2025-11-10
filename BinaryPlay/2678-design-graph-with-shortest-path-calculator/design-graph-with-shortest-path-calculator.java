1809b891771756a675811fea0934713036bba0fb
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-10 19:49:30 +0530

    Time: 63 ms (99.46%) | Memory: 55.2 MB (86.96%) - LeetSync

--- BinaryPlay/2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.java
+++ BinaryPlay/2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.java
@@ -1,52 +1,74 @@
 class Graph {
     List<List<int[]>> adj; 
+    int[][] grid;
     int N ;
     public Graph(int n, int[][] edges) { 
         N = n; 
-        adj = new ArrayList<>();
+        grid = new int[n][n];
         for(int i = 0 ; i < n ; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] edge : edges){ 
+            Arrays.fill(grid[i] , (int)1e9);
+            grid[i][i] = 0;
+        }  
+
+        for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
             int cost = edge[2]; 
-            adj.get(u).add(new int[]{v , cost});
+            grid[u][v] = cost;
+        }
+        
+        // using floyd warshell algo 
+        //store MultiSource shortest distance from all vertex to all other vertices
+        for(int vertex = 0 ; vertex < n ; vertex++){
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < n ; j++){
+                    grid[i][j] = Math.min(grid[i][j] , grid[i][vertex] + grid[vertex][j]);
+                }
+            }
         }
     }
     
     public void addEdge(int[] edge) {
-        adj.get(edge[0]).add(new int[]{edge[1], edge[2]});
-    } 
-    public int path(int source , int dest){ 
-        //store shortest distance from source to all other vertices
-        int[] res = new int[N]; 
-        Arrays.fill(res , Integer.MAX_VALUE);
-        res[source] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]); // {cost , v}
-        pq.add(new int[]{0 , source});
-
-        while(!pq.isEmpty()){
-            int[] cell = pq.poll();
-            int cost = cell[0];
-            int u = cell[1];
-            for(int[] p : adj.get(u)){
-                int v = p[0];
-                int newCost = p[1];
-
-                if(newCost + cost < res[v]){
-                    res[v] = newCost + cost;
-                    pq.add(new int[]{newCost + cost  , v});
-                }
-
+        int u = edge[0];
+        int v = edge[1];
+        int cost = edge[2];  
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                grid[i][j] = Math.min(grid[i][j] , grid[i][u] + cost + grid[v][j]);
             }
         }
-        return res[dest];
-    }
+    } 
+    // using dijkstra algo
+    // public int path1(int source , int dest){ 
+    //     //store shortest distance from source to all other vertices
+    //     int[] res = new int[N]; 
+    //     Arrays.fill(res , Integer.MAX_VALUE);
+    //     res[source] = 0;
+
+    //     PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]); // {cost , v}
+    //     pq.add(new int[]{0 , source});
+
+    //     while(!pq.isEmpty()){
+    //         int[] cell = pq.poll();
+    //         int cost = cell[0];
+    //         int u = cell[1];
+    //         for(int[] p : adj.get(u)){
+    //             int v = p[0];
+    //             int newCost = p[1];
+
+    //             if(newCost + cost < res[v]){
+    //                 res[v] = newCost + cost;
+    //                 pq.add(new int[]{newCost + cost  , v});
+    //             }
+
+    //         }
+    //     }
+    //     return res[dest];
+    // } 
     public int shortestPath(int node1, int node2) {
-        int dist = path(node1 , node2); 
-        return (dist == Integer.MAX_VALUE) ? -1 : dist;
+        // int dist = path1(node1 , node2);  //using dijkestra 
+        int dist = grid[node1][node2]; // using floyd warshell algo 
+        return (dist == (int)1e9) ? -1 : dist;
     }
 }
 
