4e84a7dfbc71f6aac37a553e979535cc04d17e0c
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-22 18:38:23 +0530

    Time: 8 ms (12.13%) | Memory: 45.2 MB (39.36%) - LeetSync

---
+++ BinaryPlay/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) { 
+
+        int ans = 0;
+        Stack<Integer> st = new Stack<>();
+
+        for(String str : tokens){ 
+            if(!str.equals("*") && !str.equals("/") && !str.equals("+") && !str.equals("-")){
+                int num = Integer.parseInt(str);
+                st.add(num);
+            }else{ 
+                int num2 = st.pop();
+                int num1 = st.pop();
+                if(str.equals("+")){
+                    ans = num1 + num2;
+                }else if(str.equals("-")){
+                    ans = num1 - num2;
+                }else if(str.equals("*")){
+                    ans = num1 * num2;
+                }else if(str.equals("/")){
+                    ans = num1 / num2;
+                }
+                st.add(ans);
+            }
+        }
+        return st.peek();
+    }
+}
