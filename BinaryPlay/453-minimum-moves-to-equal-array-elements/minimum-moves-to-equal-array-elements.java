124b96df21ea6f3081e41efeec189581b956e912
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-22 16:40:21 +0530

    Time: 3 ms (33.64%) | Memory: 46.8 MB (28.58%) - LeetSync

---
+++ BinaryPlay/453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int n = nums.length;
+        /*
+            In starting we have array [1,2,3] -->[2,3,3] -->[3,4,3] -->[4,4,4]
+            so total increament is (n - 1) * number of operations
+            so final sum can be weitten as final sum = Intital sum + (n - 1) * number of operations
+
+            As at last the array will be [x,x,x....n times]
+            so final sum will be n * x
+
+            //as --> minimum element + number of operations = x  => example -> 1(miniEle) + 3(number of operations) --> 4 
+
+            so => n * x = InitialSum + (n - 1) * number of operations
+            =>  (minimum element + number of operations) * n = InitialSum + (n - 1) * number of operations
+            => lets take number of operations = m
+            => min * n + m * n = InitialSum + n * m - m;
+            => min * n = InitialSum  - m;
+
+            =>[ m = InitialSum - min * n ] ==> last equation that we got, where m is number of operations 
+        */
+
+        int minEle = Integer.MAX_VALUE;
+        int intitalSum = 0;
+        for(int i = 0 ; i < n ; i++){
+            minEle = Math.min(minEle , nums[i]);
+            intitalSum += nums[i];
+        }
+
+        return intitalSum - minEle * n;
+    }
+}
