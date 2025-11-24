d5292422b7fec77fe1e418bd349aa3c943087244
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-24 18:23:15 +0530

    Time: 10 ms (68.02%) | Memory: 49.7 MB (37.93%) - LeetSync

--- BinaryPlay/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ BinaryPlay/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,24 +1,25 @@
-class Solution {
-    int n ;
-    int[][] dp;
-    public int solve(int i , int rem , int[] nums){
-        if(i >= n){
-            if(rem == 0) return 0;
-            else return Integer.MIN_VALUE; 
-        }
-        if(dp[i][rem] != -1) return dp[i][rem];
-
-        int take = nums[i] + solve(i + 1 , (rem + nums[i]) % 3 , nums);
-        int skip = solve(i + 1 , rem , nums);
-
-        return dp[i][rem] = Math.max(take , skip);
-    }
+class Solution { 
     public int maxSumDivThree(int[] nums) {
-        n = nums.length;
-        dp = new int[n + 1][3];
-        for(int[] r : dp) Arrays.fill(r , -1);
-        
-        // Dynamic Programming approach 
-        return solve(0 , 0 , nums);
+        int n = nums.length;
+        int[][] t = new int[n + 1][3]; 
+        // t[0][rem] ==> stores sum from 0 to n- 1  with reminder rem
+
+        for(int i = 0 ; i <= n ; i++){
+            Arrays.fill(t[i] , Integer.MIN_VALUE);
+        } 
+
+        t[n][0] = 0 ; // index outofbound and rem = 0 
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int rem = 0 ; rem < 3 ; rem++){
+                int newRem = (rem + nums[i]) % 3;
+                int take = nums[i] + t[i + 1][newRem];
+                int skip = t[i + 1][rem];
+
+                t[i][rem] = Math.max(take , skip);
+            }
+        }
+
+        return t[0][0] ;
     }
 }
