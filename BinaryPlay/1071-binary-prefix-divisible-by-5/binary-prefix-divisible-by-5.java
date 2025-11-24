1f24812a6beac402176e498fb16d213ce55b610a
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-24 17:03:35 +0530

    Time: 4 ms (21.31%) | Memory: 47.7 MB (16.73%) - LeetSync

--- BinaryPlay/1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
+++ BinaryPlay/1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
-        List<Boolean> ans = new ArrayList<>();
+        int n = nums.length;
+        List<Boolean> ans = new ArrayList<>(); 
         int num = 0;
 
-        for(int n : nums){
-            num = (num * 2 + n) % 5;
+        for(int i = 0 ; i < n ; i++){ 
+            num = num * 2; // create an extra space
+            num += nums[i]; // add nums[i]
+            num = num % 5;
             ans.addLast(num == 0);
         }
         return ans;
