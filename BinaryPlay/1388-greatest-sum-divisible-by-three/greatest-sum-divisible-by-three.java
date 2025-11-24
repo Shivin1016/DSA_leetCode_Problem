0da7625186c87255b3f910ddc7d5948e1b74a50e
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-24 18:04:31 +0530

    Time: 15 ms (45.77%) | Memory: 54.4 MB (9.40%) - LeetSync

--- BinaryPlay/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ BinaryPlay/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,43 +1,24 @@
 class Solution {
+    int n ;
+    int[][] dp;
+    public int solve(int i , int rem , int[] nums){
+        if(i >= n){
+            if(rem == 0) return 0;
+            else return Integer.MIN_VALUE; 
+        }
+        if(dp[i][rem] != -1) return dp[i][rem];
+
+        int take = nums[i] + solve(i + 1 , (rem + nums[i]) % 3 , nums);
+        int skip = solve(i + 1 , rem , nums);
+
+        return dp[i][rem] = Math.max(take , skip);
+    }
     public int maxSumDivThree(int[] nums) {
-        int n = nums.length;
-
-        List<Integer> rem_1 = new ArrayList<>();
-        List<Integer> rem_2 = new ArrayList<>();
-
-        int totalSum = 0;
-
-        for(int i = 0 ; i < n ; i++){ 
-            if(nums[i] % 3 == 1){
-                rem_1.add(nums[i]);
-            }else if(nums[i] % 3 == 2){
-                rem_2.add(nums[i]);
-            }
-            totalSum += nums[i];
-        }
-
-        int reminder = totalSum % 3 ;
-        if(reminder == 0) return totalSum;
-
-        Collections.sort(rem_1);
-        Collections.sort(rem_2);
-
-        int res = 0;
-        int size1 = rem_1.size();
-        int size2 = rem_2.size();
-
-        //reminder == 1 
-        if(reminder == 1){
-            int remove1 = size1 >= 1 ? rem_1.get(0) : Integer.MAX_VALUE;
-            int remove2 = size2 >= 2 ? rem_2.get(0) + rem_2.get(1) : Integer.MAX_VALUE; 
-            res = Math.max(res , totalSum - Math.min(remove1 , remove2)); 
-        }else if(reminder == 2){
-            int remove1 = size2 >= 1 ? rem_2.get(0) : Integer.MAX_VALUE;
-            int remove2 = size1 >= 2 ? rem_1.get(0) + rem_1.get(1) : Integer.MAX_VALUE; 
-            
-            res = Math.max(res , totalSum - Math.min(remove1 , remove2));  
-        }
-
-        return res;
+        n = nums.length;
+        dp = new int[n + 1][3];
+        for(int[] r : dp) Arrays.fill(r , -1);
+        
+        // Dynamic Programming approach 
+        return solve(0 , 0 , nums);
     }
 }
