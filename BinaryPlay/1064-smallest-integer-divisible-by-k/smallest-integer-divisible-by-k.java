2f7a497a95230fc1a1b0aeea47c88c9903fe5e59
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-25 09:19:36 +0530

    Time: 2 ms (97.52%) | Memory: 42.2 MB (27.56%) - LeetSync

--- BinaryPlay/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
+++ BinaryPlay/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -1,18 +1,20 @@
 class Solution {
+    // 1111 / 4 ==> never gives you 0 reminder 
     public int smallestRepunitDivByK(int k) {
-        if(k == 1) return 1;
-        //jo even number hai unko return kar do -1
+        if(k == 1) return 1; // base 1 
+
+        //jo even number hai unko return kar do -1  or any number that multiple of 5 or 5 
         if(k % 2 == 0 || k % 5 == 0) return -1;
 
-        //jo remaing 3 , 7 , 9 and uske multiples hai  
+        //jo remaing number  3 , 7 , 9 and uske multiples hai   
 
-        int rem = 0;
-        int count = 0;
+        int rem = 0;  
+        int count = 0; // size of the number which contains only 1 digit 
         while(count <= k){ //count goes greater then k then we could not found that number 
-            count++;
-            rem = ((rem * 10) + 1 ) % k;
+            count++; 
+            rem = ((rem * 10) + 1 ) % k; 
             if(rem == 0){
-                return count;
+                return count; // size of the number which contains only 1 digit
             }
         }
  
