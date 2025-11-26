0925381d9fc86209d64d69436190a785041bd2f7
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-26 20:08:55 +0530

    Time: 54 ms (77.03%) | Memory: 98.7 MB (17.56%) - LeetSync

--- BinaryPlay/2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ BinaryPlay/2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,7 +1,7 @@
 class Solution {
     int n , m ; 
     int K ;
-    long modulas = 1000000007;
+    long M = 1000000007;
     int[][][] dp ;
     public int solve(int[][] grid , int i , int j , int mod ){
         if(i >= m || j >= n ) return 0;
@@ -20,18 +20,42 @@ class Solution {
         path1 += solve(grid , i + 1 , j , newMod);
         path2 += solve(grid , i , j + 1 , newMod);
 
-        return dp[i][j][newMod] = (int)((path1 + path2) % modulas);
+        return dp[i][j][newMod] = (int)((path1 + path2) % M);
     }
     public int numberOfPaths(int[][] grid, int k) {
         m = grid.length;
         n = grid[0].length;
         K = k;
-        dp = new int[m + 1][n + 1][k];
-        for(int[][] r : dp){ 
-            for(int[] t : r){
-                Arrays.fill(t , -1);
+        // dp = new int[m + 1][n + 1][k];
+        // for(int[][] r : dp){ 
+        //     for(int[] t : r){
+        //         Arrays.fill(t , -1);
+        //     }
+        // }
+        // return solve(grid , 0 , 0 , 0); 
+
+
+        // bottom up approach
+        int[][][] t = new int[m + 1][n + 1][K];
+
+        for(int remain = 0 ; remain < k ; remain++){
+            t[m - 1][n - 1][remain] = (remain + grid[m - 1][n - 1]) % k == 0 ? 1 : 0;
+        }
+
+        for(int i = m - 1 ; i >= 0 ; i--){
+            for(int j = n - 1 ; j >= 0 ; j--){
+                for(int remain = 0 ; remain < k ; remain++){
+                    if(i == m - 1 && j == n - 1) continue;
+
+                    int R = (remain + grid[i][j]) % k;
+                    long p = 0;
+                    p += t[i + 1][j][R];
+                    p += t[i][j + 1][R];
+
+                    t[i][j][remain] = (int)(p % M);
+                }
             }
         }
-        return solve(grid , 0 , 0 , 0); 
+        return t[0][0][0];
     }
 }
