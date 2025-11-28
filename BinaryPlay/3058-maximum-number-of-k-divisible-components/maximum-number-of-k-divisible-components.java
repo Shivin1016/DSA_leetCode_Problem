4bc180042c4818dfdfdb8c40fa4ed5b58603eba2
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-28 09:17:49 +0530

    Time: 26 ms (67.74%) | Memory: 92.3 MB (18.28%) - LeetSync

--- BinaryPlay/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ BinaryPlay/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -1,11 +1,11 @@
 class Solution {
     int componentCnt = 0;
     public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        // adjencyList --> 
         List<List<Integer>> adjList = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             adjList.add(new ArrayList<>());
         }
-
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
@@ -14,7 +14,7 @@ class Solution {
             adjList.get(v).add(u);
         }
 
-        DFS(adjList , 0 , -1 , values , k);
+        DFS(adjList , 0 , -1 , values , k); // DFs 
         return componentCnt ;
     }
 
@@ -30,8 +30,8 @@ class Solution {
         //reached to the leaf node then check for sum
         sum += values[currNode];
         if(sum % k == 0){ // sum is divisible by k then add with componentCount and make sum 0 to return it root node , and return sum as it is
-            componentCnt++;
-            sum = 0;
+            componentCnt++; // increase compponet cnt 
+            sum = 0; // make sum = 0
         }
         return sum;
     }
