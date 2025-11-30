1add76faa48c5f416604d5b6a8ffc4764e989a05
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-11-30 16:43:48 +0530

    Time: 28 ms (57.58%) | Memory: 92 MB (61.54%) - LeetSync

---
+++ BinaryPlay/1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        long total = 0;
+        for(int num : nums) total += num;
+
+        int target = (int)(total % p);
+        if(target == 0) return 0; 
+        if(total < p) return -1;
+        
+        int minLen = Integer.MAX_VALUE; 
+        Map<Integer , Integer> mp = new HashMap<>() ; // store previus sum from 0 to i
+        
+        int currSum = 0; 
+        mp.put(0 , -1); // intitally currSum
+        for(int j = 0 ; j < n ; j++){   
+            currSum = (currSum + nums[j]) % p; /// store module commulative sum 0 to j
+
+            int find = (currSum - target + p) % p;
+            if(mp.containsKey(find)){
+                int i = mp.get(find); // commulative sum from 0 to i
+                minLen = Math.min(minLen , j - i);
+            }
+            mp.put(currSum , j);
+        } 
+        return minLen == n ? -1 : minLen;
+    }
+}
