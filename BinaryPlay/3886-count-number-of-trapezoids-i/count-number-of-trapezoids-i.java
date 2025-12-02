f5fc2b5bce00ce7b5984ad2695e84e30b294c11b
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-02 10:03:55 +0530

    Time: 37 ms (80.10%) | Memory: 206.5 MB (21.94%) - LeetSync

--- BinaryPlay/3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
+++ BinaryPlay/3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -9,22 +9,13 @@ class Solution {
             mp.put(y , mp.getOrDefault(y , (long)0) + 1);
         }
 
-        int size = mp.size(); 
-        List<Long> pairs = new ArrayList<>();
+        int size = mp.size();  
+        long count = 0; 
+        long sum = 0  ;
         for(long cnt : mp.values()){
-            long pair = ((cnt * (cnt - 1)) % mod / 2) % mod;
-            pairs.add(pair);
-        }
-
-        long[] prefixSum = new long[size];
-        prefixSum[0] = pairs.get(0);
-        for(int i = 1 ; i < size ; i++){
-            prefixSum[i] = (prefixSum[i - 1] + pairs.get(i)) % mod;
-        }
-
-        long count = 0;  
-        for(int i = 0 ; i < size - 1 ; i++){
-            count = (count % mod + (pairs.get(i) * (prefixSum[size - 1] - prefixSum[i])) % mod) % mod;
+            long pair = (cnt * (cnt - 1)) % mod / 2;
+            count = (count % mod + (pair * sum)) % mod;
+            sum += pair;
         }
 
         return (int)(count % mod);
