394ff180786ce28bf37d6310d58c63b8c05b46c4
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-10 00:29:49 +0530

    Time: 303 ms (8.53%) | Memory: 189.2 MB (43.41%) - LeetSync

--- BinaryPlay/3885-count-special-triplets/count-special-triplets.java
+++ BinaryPlay/3885-count-special-triplets/count-special-triplets.java
@@ -1,17 +1,18 @@
 class Solution {
     public int specialTriplets(int[] nums) {
-        int n = nums.length;
+        int n = nums.length; 
         long count = 0; 
+
         long M = 1000_000_007;
 
-        Map<Integer , Long> leftMp = new HashMap<>();
-        leftMp.put(nums[0] , (long)1);
+        Map<Integer , Long> leftMp = new HashMap<>(); 
+        leftMp.put(nums[0] , (long)1); // zeroth idx element stores  
 
         Map<Integer , Long> rightMp = new HashMap<>();
-        // /store all values to rightMap
+        // /store  from 1 to n - 1 to rightMap
         for(int i = 1 ; i < n ; i++) rightMp.put(nums[i] , rightMp.getOrDefault(nums[i] ,(long)0) + 1);
 
-        for(int j = 1 ; j < n - 1 ; j++){
+        for(int j = 1 ; j < n - 1 ; j++){ 
             int doubleNum = nums[j] * 2;
 
             if(leftMp.containsKey(doubleNum) && rightMp.containsKey(doubleNum)){
