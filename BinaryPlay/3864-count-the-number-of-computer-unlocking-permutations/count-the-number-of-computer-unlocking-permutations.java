b081b194c5bb928296cba5c01ba8ae4bd5428d89
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-10 22:33:25 +0530

    Time: 2 ms (74.67%) | Memory: 92.3 MB (5.33%) - LeetSync

--- BinaryPlay/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ BinaryPlay/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -3,22 +3,16 @@ class Solution {
     long M = 1000_000_007;
     public int countPermutations(int[] complexity) {
         int n = complexity.length;
+        long count = 1;
 
         //check that if root computer is equal to any other computer then return 0
         for(int i = 1 ; i < n ; i++){ 
-            if(complexity[0] == complexity[i]){
+            if(complexity[0] >= complexity[i]){
                 return 0;
-            }
-            if(complexity[0] > complexity[i]) return 0;
-        }  
-        
-        long count = factorial(n - 1) % M;
+            } 
+            count = (count * i) % M;
+        }   
+
         return (int)count;
-    }
-
-    public long factorial(int n){
-        if(n == 0 || n == 1) return 1;
-
-        return (factorial(n - 1) * n) % M;
-    }
+    } 
 }
