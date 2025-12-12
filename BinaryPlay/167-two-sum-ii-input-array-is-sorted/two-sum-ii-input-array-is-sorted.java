edc7895646f7c3f2dce3812d1fbfe9613f542153
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-12 18:21:58 +0530

    Time: 2 ms (95.50%) | Memory: 48.7 MB (6.18%) - LeetSync

--- BinaryPlay/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ BinaryPlay/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,32 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int n = numbers.length;
-        
-        for(int i = 0 ; i < n ; i++){
-            int remain = target - numbers[i];
-            int idx = binarySearch(numbers , remain , n);
-            if(idx != -1){
-                return new int[]{i + 1 , idx + 1};
+        int low = 0 ;
+        int high = n - 1;
+
+        while(low < high){
+            int sum = numbers[low] + numbers[high]; 
+
+            if(sum == target){
+                return new int[]{low + 1 , high + 1};
+            }else if(sum < target){
+                low++;
+            }else{
+                high--;
             }
         }
         return new int[]{};
-    }
-
-    public int binarySearch(int[] nums , int target , int n){
-        int low = 0;
-        int high = n - 1; 
-        int res = -1;
-        while(low <= high){
-            int mid  = low + (high - low) / 2;
-            if(nums[mid] == target){
-                res = mid;
-                low = mid + 1;
-            }
-            else if(nums[mid] < target) low = mid + 1;
-            else{
-                high = mid - 1;
-            }
-        }
-        return res;
-    }
+    } 
 }
