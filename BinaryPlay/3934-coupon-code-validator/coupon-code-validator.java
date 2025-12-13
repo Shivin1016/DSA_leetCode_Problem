990808da4dd290f304231c2355ad7998fb7cdd6f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-13 07:59:10 +0530

    Time: 21 ms (32.27%) | Memory: 47.4 MB (47.27%) - LeetSync

---
+++ BinaryPlay/3934-coupon-code-validator/coupon-code-validator.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isValid(String code , String business){
+        HashSet<String> businessLines = new HashSet<>();
+        businessLines.add("electronics");
+        businessLines.add("grocery");
+        businessLines.add("pharmacy");
+        businessLines.add("restaurant");
+
+        for(char ch : code.toCharArray()){
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || (ch == '_')){ 
+                continue;
+            }else return false;
+        }
+
+        if(businessLines.contains(business)){
+            return true;
+        }
+
+        return false;
+    }
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String[]> ans = new ArrayList<>();
+        int n = code.length;
+
+        for(int i = 0 ; i < n ; i++){ 
+            String business = businessLine[i];
+            if(code[i].length() == 0) continue;
+            
+            if(isValid(code[i] , business) && isActive[i]){
+                System.out.println(code[i]);
+                ans.add(new String[]{code[i] , business});
+            }
+        } 
+
+        System.out.println(ans.size());
+
+        // sort list on basis of bsuiess line
+        Collections.sort(ans , (a , b) ->{ 
+            int val = a[1].compareTo(b[1]);
+            if(val == 0){
+                return a[0].compareTo(b[0]);
+            }
+            return val;
+        });
+ 
+
+        List<String> result = new ArrayList<>();
+        for(String[] ls : ans){
+            result.add(ls[0]);
+        }
+        return result;
+    }
+}
