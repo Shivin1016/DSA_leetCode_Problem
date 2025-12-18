d713e72f86a54f235239b92fa6a0f9fe3eb7a272
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-18 17:24:24 +0530

    Time: 10 ms (7.57%) | Memory: 101 MB (49.67%) - LeetSync

---
+++ BinaryPlay/3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
@@ -0,0 +1,42 @@
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+
+        long[] profit = new long[n];
+        long totalProfit = 0;
+        for(int i = 0 ; i < n ; i++){
+            profit[i] = (long)(prices[i] * strategy[i]);
+            totalProfit += profit[i];
+        }
+
+        int i = 0 , j = 0;
+        long maxGain = 0;
+        long originalProfit = 0;
+        long modifiedProfit = 0; 
+
+        while(j < n){
+            originalProfit += profit[j];
+            
+            //agar ham window of k ke right half part me a gaye then add price value
+            if((j - i + 1) > k / 2){
+                modifiedProfit += prices[j];
+            }
+            //out of window --> window shrink kro
+            if((j - i + 1) > k){
+                modifiedProfit -= prices[i + k / 2]; //at half index 
+                originalProfit -= profit[i];
+                i++;
+            }
+            
+            if(j - i + 1 == k){
+                //got subarray of k size 
+                maxGain = Math.max(maxGain , modifiedProfit - originalProfit);
+            }
+
+            j++;
+        }
+        System.out.println(maxGain);
+        long ans = totalProfit + (maxGain < 0 ? 0 : maxGain);
+        return totalProfit + (maxGain < 0 ? 0 : maxGain);
+    }
+}
