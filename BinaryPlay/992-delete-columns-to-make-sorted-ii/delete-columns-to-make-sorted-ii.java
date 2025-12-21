be3c1be2481c56c83345ec3d6cbe3ddd472680be
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-21 23:03:48 +0530

    Time: 1 ms (78.08%) | Memory: 43.3 MB (73.97%) - LeetSync

--- BinaryPlay/992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ BinaryPlay/992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -4,32 +4,32 @@ class Solution {
         int cols = strs[0].length();
 
         // sorted[i] = true, means strs[i] < strs[i+1] is already fixed
-        boolean[] sorted = new boolean[rows-1];
-        int ans = 0;
+        boolean[] alreadySorted = new boolean[rows-1];
+        int deletion = 0;
 
-        for(int col=0; col<cols; col++) {
-            boolean needDelete = false;
+        for(int col = 0 ; col < cols; col++) {
+            boolean canDelete = false;
 
-            for(int row=0; row<rows-1; row++) {
-                if(!sorted[row] && strs[row].charAt(col) > strs[row+1].charAt(col)) {
-                    needDelete = true;
+            for(int row = 0 ; row < rows - 1 ; row++) {
+                if(!alreadySorted[row] && strs[row].charAt(col) > strs[row + 1].charAt(col)){
+                    canDelete = true;
+                    deletion++;
                     break;
                 }
             }
 
-            if(needDelete) {
-                ans++;
+            if(canDelete) { 
                 continue;  // skip updating sorted[], move to next column
             }
 
             // Update sorted pairs where this column fixes order
-            for(int row=0; row<rows-1; row++) {
-                if(!sorted[row] && strs[row].charAt(col) < strs[row+1].charAt(col)) {
-                    sorted[row] = true;
-                }
+            for(int row= 0 ; row < rows-1 ; row++) {
+                // if(!alreadySorted[row] && strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+                // }
+                    alreadySorted[row] = alreadySorted[row] | strs[row].charAt(col) < strs[row + 1].charAt(col);
             }
         }
 
-        return ans;
+        return deletion;
     }
 } 
