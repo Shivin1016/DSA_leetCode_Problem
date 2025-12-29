75edbd892636fbe73efc2e606a79e92591cb222b
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2025-12-29 23:38:14 +0530

    Time: 341 ms (59.87%) | Memory: 143.9 MB (5.10%) - LeetSync

--- BinaryPlay/757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ BinaryPlay/757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,23 +1,31 @@
 class Solution {
+    Map<String , Boolean> t = new HashMap<>();
     public boolean solve(StringBuilder curr_bottom  , StringBuilder next_above ,Map<String , List<Character>> left_right_and_top , int i){
         if(curr_bottom.length() == 1) return true;
 
-        if(i == curr_bottom.length() - 1){
-            return solve(next_above , new StringBuilder() ,left_right_and_top  , 0);
+        String key = curr_bottom.toString() + "_" + i + "_" + next_above.toString();
+        if(t.containsKey(key)) return t.get(key);
+
+        if(i == (curr_bottom.length() - 1)){
+            boolean res =  solve(next_above , new StringBuilder() ,left_right_and_top  , 0);
+            t.put(key , res);
+            return res;
         }
 
         String pair = curr_bottom.substring(i , i + 2);
         if(!left_right_and_top.containsKey(pair)){
+            t.put(key , false);
             return false;
         }
         for(char ch : left_right_and_top.get(pair)){
             next_above.append(ch);
             if(solve(curr_bottom , next_above , left_right_and_top , i + 1) == true){
+                t.put(key , true);
                 return true;
             }
             next_above.deleteCharAt(next_above.length() - 1);
         }
-
+        t.put(key , false);
         return false;
     }
     public boolean pyramidTransition(String bottom, List<String> allowed) { 
