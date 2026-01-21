9ebbc3904b9e418b212f2b34ae39a9f9df5c03ca
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-01-21 08:17:43 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (8.33%) - LeetSync

---
+++ BinaryPlay/3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+
+        int[] ans = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            int prev = -1;
+            int prime = nums.get(i);
+            for(int j = 0 ; j < 32 ; j++){
+                if((prime & (1 << j)) > 0) continue;
+                prev = j - 1;
+                break;
+            }
+            if(prev == -1) ans[i] = -1;
+            else{
+                ans[i] = (prime ^ (1 << prev));
+            }
+        }
+
+        return ans;
+    }
+}
