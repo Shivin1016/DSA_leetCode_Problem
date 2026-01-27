e3e1feaf48cfca724eb3daa74fb3c0134a16ee30
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-01-27 18:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (38.24%) - LeetSync

--- BinaryPlay/62-unique-paths/unique-paths.java
+++ BinaryPlay/62-unique-paths/unique-paths.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-
-        //botttom approach
-        int[][] t = new int[m + 1][n + 1];
-        // t[i][j] --> totla number of ways to reach (i , j) from (0 , 0)
-
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i == 0 || j == 0){
-                    t[i][j] = 1;
-                }
-                else{
-                    t[i][j] = t[i - 1][j] + t[i][j - 1];
-                }
-            }
+    int[][] t;
+    private int solve(int i , int j , int m , int n){
+        if(i < 0 || j < 0 || i == m || j == n){
+            return 0;
         }
 
-        return t[m - 1][n - 1];
+        if(i == m - 1 && j == n - 1) return 1;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int right = solve(i , j + 1 , m , n);
+        int down = solve(i + 1 , j , m , n);
+
+        return t[i][j] = (right + down);
+    } 
+    public int uniquePaths(int m, int n) {
+        t = new int[m][n];
+        for(int[] r : t) Arrays.fill(r , -1);
+
+        return solve(0 , 0 , m , n);
     }
 }
