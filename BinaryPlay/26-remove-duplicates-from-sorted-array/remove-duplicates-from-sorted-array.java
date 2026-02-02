a79ed09daa86d8b140c427d1daa3212c3036997f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-02 22:06:11 +0530

    Time: 1 ms (77.22%) | Memory: 46.6 MB (88.73%) - LeetSync

--- BinaryPlay/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ BinaryPlay/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-
+        int n = nums.length; 
         int i = 0 , j = 1;
 
         while(i < n && j < n){
-            if(nums[i] == nums[j]) j++;
-            else{
+            if(nums[i] != nums[j]){
                 i++;
                 nums[i] = nums[j];
             }
+            j++;
         }
+
         return i + 1;
     }
 }
