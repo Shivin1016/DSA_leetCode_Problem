ff452f272776f0d14c32bde2209be3834d11d32f
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-02 21:53:26 +0530

    Time: 2 ms (41.74%) | Memory: 94.5 MB (65.01%) - LeetSync

--- BinaryPlay/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ BinaryPlay/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-
-        int[] nextGreaterElem = new int[n]; 
+ 
         int maxEle = prices[n - 1];
         int cost = 0;
+
         for(int i = n - 2 ; i >= 0 ; i--){
             maxEle = Math.max(prices[i] , maxEle);
             cost = Math.max(cost , maxEle - prices[i]);
