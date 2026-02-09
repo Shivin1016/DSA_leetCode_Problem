2872160c99b25c22a469854b41a9c0a2fa1b6130
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-09 06:31:53 +0530

    Time: 410 ms (72.71%) | Memory: 0B (100.00%) - LeetSync

--- BinaryPlay/177-nth-highest-salary/nth-highest-salary.sql
+++ BinaryPlay/177-nth-highest-salary/nth-highest-salary.sql
@@ -1,5 +1,6 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
     --   1-> way
@@ -14,13 +15,19 @@ BEGIN
     --   LIMIT 1
 
     -- 2 -> way
-        SELECT salary as getNthHighestSalary
-        FROM Employee e1
-        WHERE N - 1 = (
-            SELECT COUNT(DISTINCT salary)
-            FROM Employee e2
-            WHERE e2.salary > e1.salary
-        )
-        LIMIT 1
+        -- SELECT salary as getNthHighestSalary
+        -- FROM Employee e1
+        -- WHERE N - 1 = (
+        --     SELECT COUNT(DISTINCT salary)
+        --     FROM Employee e2
+        --     WHERE e2.salary > e1.salary
+        -- )
+        -- LIMIT 1
+    
+    -- 3 -> way-> for we have to set n = n - 1
+        SELECT DISTINCT salary
+        FROM Employee 
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
   );
 END
