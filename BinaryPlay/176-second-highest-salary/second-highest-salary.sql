2123f3eccde1841c354da7efcf80109864c2fb18
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-09 05:32:12 +0530

    Time: 312 ms (14.38%) | Memory: 0B (100.00%) - LeetSync

--- BinaryPlay/176-second-highest-salary/second-highest-salary.sql
+++ BinaryPlay/176-second-highest-salary/second-highest-salary.sql
@@ -8,14 +8,25 @@
 -- ) AS SecondHighestSalary;
 
 -- 2-> way
-SELECT(
+-- SELECT(
+--     SELECT DISTINCT salary
+--     FROM (
+--         SELECT 
+--             *,
+--             DENSE_RANK() OVER(ORDER BY salary DESC) as 'ranking'
+--         FROM Employee
+--     ) as sub
+--     WHERE ranking = 2
+-- ) AS SecondHighestSalary;
+
+-- 3-> way
+SELECT (
     SELECT DISTINCT salary
-    FROM (
-        SELECT 
-            *,
-            DENSE_RANK() OVER(ORDER BY salary DESC) as 'ranking'
-        FROM Employee
-    ) as sub
-    WHERE ranking = 2
+    FROM Employee
+    WHERE salary < (
+        SELECT MAX(salary) FROM Employee
+    )
+    ORDER BY salary DESC
+    LIMIT 1 
 ) AS SecondHighestSalary;
 
