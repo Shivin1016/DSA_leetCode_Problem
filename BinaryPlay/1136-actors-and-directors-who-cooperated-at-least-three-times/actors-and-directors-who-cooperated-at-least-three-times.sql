9d748a1e948cad957fa38da00108a702beda61b9
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-09 20:45:28 +0530

    Time: 361 ms (47.38%) | Memory: 0B (100.00%) - LeetSync

--- BinaryPlay/1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ BinaryPlay/1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,16 @@
 # Write your MySQL query statement below
 
+-- 1-> way
+-- SELECT actor_id , director_id
+-- FROM ActorDirector 
+-- GROUP BY actor_id , director_id
+-- HAVING COUNT(timestamp) >= 3;
+
+-- 2 -> way
 SELECT actor_id , director_id
-FROM ActorDirector 
-GROUP BY actor_id , director_id
-HAVING COUNT(timestamp) >= 3;
+FROM(
+    SELECT actor_id , director_id , COUNT(timestamp) as cnt
+    FROM ActorDirector
+    GROUP BY actor_id , director_id
+) as s
+WHERE s.cnt >= 3;
