685ddc6ca56414988f018c46379eea203fd4c74d
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-09 06:11:37 +0530

    Time: 1146 ms (7.72%) | Memory: 0B (100.00%) - LeetSync

--- BinaryPlay/184-department-highest-salary/department-highest-salary.sql
+++ BinaryPlay/184-department-highest-salary/department-highest-salary.sql
@@ -1,33 +1,31 @@
-# Write your MySQL query statement below
--- SELECT *
--- FROM(
---     SELECT d.name as Department , e.name as  Employee , MAX(e.salary) as Salary
---     FROM Employee e
---     Inner JOIN Department d
---     ON e.departmentId = d.id
---     GROUP BY d.name 
+# Write your MySQL query statement below 
 
--- ) as sub
+-- 1--> way
+-- SELECT s.Department , s.Employee , s.Salary
+-- FROM (
+--     SELECT 
+--         *,
+--         DENSE_RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) as 'ranking'
+--     FROM (
+--         SELECT d.name as Department , e.name as  Employee , e.salary as Salary
+--         FROM Employee e
+--         Inner JOIN Department d
+--         ON e.departmentId = d.id
+--     ) as sub 
+-- ) as s
+-- WHERE ranking = 1; 
+
+-- 2 --> way
+SELECT d.name as Department , e.name as  Employee , e.salary as Salary
+FROM Employee e
+Inner JOIN Department d
+ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(SALARY)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
 
 
--- SELECT d.name as Department , e.name as  Employee , MAX(e.salary) as Salary
--- FROM Employee e
--- Inner JOIN Department d
--- ON e.departmentId = d.id
--- GROUP BY d.name ;
-
-SELECT s.Department , s.Employee , s.Salary
-FROM (
-    SELECT 
-        *,
-        DENSE_RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) as 'ranking'
-    FROM (
-        SELECT d.name as Department , e.name as  Employee , e.salary as Salary
-        FROM Employee e
-        Inner JOIN Department d
-        ON e.departmentId = d.id
-    ) as sub 
-) as s
-WHERE ranking = 1; 
 
 
