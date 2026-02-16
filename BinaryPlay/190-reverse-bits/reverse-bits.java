ba08cb5aa7e73898aac07266884e6330a41e0752
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-16 16:31:19 +0530

    Time: 1 ms (60.34%) | Memory: 42.5 MB (52.75%) - LeetSync

--- BinaryPlay/190-reverse-bits/reverse-bits.java
+++ BinaryPlay/190-reverse-bits/reverse-bits.java
@@ -2,16 +2,12 @@ class Solution {
     public int reverseBits(int n) { 
 
         int ans = 0;
-        int j = 30;
+        int j = 31;
 
         for(int i = 0 ; i < 32 ; i++){
             int k = (n >> i) & 1;
             if(k == 1){
-                if(j == -1){
-                    ans = ans + (int)Math.pow(2 , 0);
-                }else{
-                    ans = ans + (2 << j);
-                }
+                ans = ans + (1 << j);
             }
             j--;
         }
