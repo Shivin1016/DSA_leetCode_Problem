4d578371a77c8a35e0b46b990bd0408e50c909a6
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-21 16:00:55 +0530

    Time: 4 ms (87.21%) | Memory: 42.2 MB (79.37%) - LeetSync

--- BinaryPlay/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ BinaryPlay/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -6,7 +6,7 @@ class Solution {
 
         if(num % 2 == 0 || num % 3 == 0) return false;
 
-        for(int i = 5 ; i * i < num ; i += 6){
+        for(int i = 5 ; i * i <= num ; i += 6){
             if(num % i == 0 || num % (i + 2) == 0) return false;
         }
 
