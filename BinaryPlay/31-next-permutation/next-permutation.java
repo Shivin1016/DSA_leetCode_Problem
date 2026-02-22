d50c66dc9a684762110408540c922a487f2e1822
SHIVANI PRAJAPATI <137745066+Shivin1016@users.noreply.github.com>
2026-02-22 19:50:42 +0530

    Time: 3 ms (3.46%) | Memory: 44.5 MB (93.12%) - LeetSync

---
+++ BinaryPlay/31-next-permutation/next-permutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    private void swap(int[] nums , int l , int r){
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+    private void reverse(int[] nums , int l , int r){
+        while(l < r){
+            swap(nums , l , r);
+            l++;
+            r--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // find number from right side which satisfied that condition (nums[i - 1] < nums[i])
+     
+        int rightIdx = -1;
+        for(int i = n - 1 ; i > 0 ; i--){
+            if(nums[i - 1] < nums[i]){ 
+                rightIdx = i - 1;
+                break;
+            }
+        }
+
+        if(rightIdx == -1){
+            reverse(nums , 0 , n - 1); // reverse whole array
+        }
+        else{
+            // find just greater number than rightNum on right side
+            int justGreater = Integer.MAX_VALUE;
+            int target = nums[rightIdx];
+            int idx = -1; 
+            for(int i = rightIdx + 1 ; i < n ; i++){
+                if(nums[i] > target && nums[i] <= justGreater){
+                    justGreater = nums[i];
+                    idx = i;
+                } 
+            }
+            // now swap 
+            System.out.println(idx);
+            swap(nums , idx , rightIdx);
+            // after that just reverse array from rightIdx + 1
+            reverse(nums , rightIdx + 1 , n - 1);
+        }
+    }
+}
